0166e117452e484fbbb1531d62d89a666062fccb
Farbod <cppox1@gmail.com>
2021-12-09 17:01:47 +0400

    migrated to androidx

--- app/src/main/java/xyz/btcland/libretube/MainActivity.kt
+++ app/src/main/java/xyz/btcland/libretube/MainActivity.kt
@@ -1,8 +1,8 @@
 package xyz.btcland.libretube
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 
