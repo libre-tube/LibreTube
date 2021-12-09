f2abfb937858ee63f4d610da49b8b742371c0120
Farbod <cppox1@gmail.com>
2021-12-09 16:31:40 +0400

    Initial commit

---
+++ app/src/main/java/xyz/btcland/libretube/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.btcland.libretube
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
