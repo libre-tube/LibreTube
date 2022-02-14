87c52f7e58155367f44eb4bef333478ba41da1cf
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2022-02-14 20:57:50 +0000

    Format file.

--- app/src/main/java/com/github/libretube/VideoViews.kt
+++ app/src/main/java/com/github/libretube/VideoViews.kt
@@ -4,19 +4,19 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 fun Long?.formatShort(): String = when {
-                                            this!!<1000 -> {
-                                                this.toString()
-                                            }
-                                            this in 1000..999999 -> {
-                                                val decimal = BigDecimal(this/1000).setScale(0, RoundingMode.HALF_EVEN)
-                                                decimal.toString()+"K"
-                                            }
-                                            this in 1000000..10000000 -> {
-                                                val decimal = BigDecimal(this/1000000).setScale(0, RoundingMode.HALF_EVEN)
-                                                decimal.toString()+"M"
-                                            }
-                                            else -> {
-                                                val decimal = BigDecimal(this/1000000).setScale(0, RoundingMode.HALF_EVEN)
-                                                decimal.toString()+"M"
-                                            }
-                                        }
+    this!! < 1000 -> {
+        this.toString()
+    }
+    this in 1000..999999 -> {
+        val decimal = BigDecimal(this / 1000).setScale(0, RoundingMode.HALF_EVEN)
+        decimal.toString() + "K"
+    }
+    this in 1000000..10000000 -> {
+        val decimal = BigDecimal(this / 1000000).setScale(0, RoundingMode.HALF_EVEN)
+        decimal.toString() + "M"
+    }
+    else -> {
+        val decimal = BigDecimal(this / 1000000).setScale(0, RoundingMode.HALF_EVEN)
+        decimal.toString() + "M"
+    }
+}
