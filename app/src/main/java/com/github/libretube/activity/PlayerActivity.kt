020a962c6ff1763860aadbab5563642a62cf9fed
Vassili Simon <vassili.simon@cronn.de>
2022-03-21 22:37:49 +0100

    Use viewbinding in PlayerActivity

--- app/src/main/java/com/github/libretube/activity/PlayerActivity.kt
+++ app/src/main/java/com/github/libretube/activity/PlayerActivity.kt
@@ -3,10 +3,14 @@ package com.github.libretube.activity
 import android.app.Activity
 import android.os.Bundle
 import com.github.libretube.R
+import com.github.libretube.databinding.ActivityPlayerBinding
 
 class PlayerActivity : Activity() {
+    private lateinit var binding: ActivityPlayerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
