e3054e28449d84dd206f2ca3061ad301ce6fad42
Bnyro <yannick.fehler@web.de>
2022-05-16 22:33:35 +0200

    Removed Import

--- app/src/main/java/com/github/libretube/SettingsActivity.kt
+++ app/src/main/java/com/github/libretube/SettingsActivity.kt
@@ -18,7 +18,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.ListPreference
