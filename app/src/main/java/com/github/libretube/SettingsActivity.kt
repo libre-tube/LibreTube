3f19d016f7374e817dc3d76a9f5c6423276f27b8
Bnyro <yannick.fehler@web.de>
2022-05-30 22:23:44 +0200

    ktlint

--- app/src/main/java/com/github/libretube/SettingsActivity.kt
+++ app/src/main/java/com/github/libretube/SettingsActivity.kt
@@ -28,13 +28,13 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import org.json.JSONObject
-import org.json.JSONTokener
-import retrofit2.HttpException
 import java.io.IOException
 import java.io.InputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
+import org.json.JSONObject
+import org.json.JSONTokener
+import retrofit2.HttpException
 
 private var isCurrentViewMainSettings = true
 private var requireMainActivityRestart = false
@@ -42,6 +42,7 @@ private var requireMainActivityRestart = false
 class SettingsActivity :
     AppCompatActivity(),
     SharedPreferences.OnSharedPreferenceChangeListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         DynamicColors.applyToActivityIfAvailable(this)
         updateAccentColor(this)
