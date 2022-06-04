83d634bc955b3ba5c49e44aa0408902838ea55ff
Bnyro <yannick.fehler@web.de>
2022-06-04 17:34:18 +0200

    sub channels rewrite

--- app/src/main/java/com/github/libretube/adapters/SubscriptionChannelAdapter.kt
+++ app/src/main/java/com/github/libretube/adapters/SubscriptionChannelAdapter.kt
@@ -1,19 +1,30 @@
 package com.github.libretube.adapters
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.RecyclerView
 import com.github.libretube.MainActivity
 import com.github.libretube.R
+import com.github.libretube.obj.Subscribe
 import com.github.libretube.obj.Subscription
+import com.github.libretube.util.RetrofitInstance
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 class SubscriptionChannelAdapter(private val subscriptions: MutableList<Subscription>) :
     RecyclerView.Adapter<SubscriptionChannelViewHolder>() {
+    val TAG = "SubChannelAdapter"
     override fun getItemCount(): Int {
         return subscriptions.size
     }
@@ -35,6 +46,31 @@ class SubscriptionChannelAdapter(private val subscriptions: MutableList<Subscrip
             val bundle = bundleOf("channel_id" to subscription.url)
             activity.navController.navigate(R.id.channel, bundle)
         }
+        val unsubscribeBtn = holder.v.findViewById<com.google.android.material.button.MaterialButton>(R.id.subs_subscribe)
+        unsubscribeBtn.setOnClickListener {
+            val channel_id = subscription.url?.replace("/channel/", "")!!
+            unsubscribe(holder.v.context, channel_id)
+        }
+    }
+
+    private fun unsubscribe(context: Context, channel_id: String) {
+        fun run() {
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = try {
+                    val sharedPref = context?.getSharedPreferences("token", Context.MODE_PRIVATE)
+                    RetrofitInstance.api.unsubscribe(
+                        sharedPref?.getString("token", "")!!,
+                        Subscribe(channel_id)
+                    )
+                } catch (e: IOException) {
+                    println(e)
+                    Log.e(TAG, "IOException, you might not have internet connection")
+                } catch (e: HttpException) {
+                    Log.e(TAG, "HttpException, unexpected response")
+                }
+            }
+        }
+        run()
     }
 }
 
