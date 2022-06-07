a1c049918892b518528687eb4e6a336f65f279df
Bnyro <yannick.fehler@web.de>
2022-06-07 21:23:25 +0200

    ktlint

--- app/src/main/java/com/github/libretube/SettingsActivity.kt
+++ app/src/main/java/com/github/libretube/SettingsActivity.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
