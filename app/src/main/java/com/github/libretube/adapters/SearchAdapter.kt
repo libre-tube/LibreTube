9ad8409fe1f9aca3dbf3476facac255e6819bfc7
Bnyro <yannick.fehler@web.de>
2022-06-08 07:58:44 +0200

    ktlint

--- app/src/main/java/com/github/libretube/adapters/SearchAdapter.kt
+++ app/src/main/java/com/github/libretube/adapters/SearchAdapter.kt
@@ -1,10 +1,7 @@
 package com.github.libretube.adapters
 
-import android.graphics.Color
-import android.graphics.Color.rgb
 import android.os.Bundle
 import android.text.format.DateUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
