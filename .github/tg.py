630216a25735ab5b15db3fe732fec7cd6a4e96ee
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-12 00:47:52 +0530

    Telegram Bot Support

---
+++ .github/tg.py
@@ -0,0 +1,11 @@
+import telegram
+from tgconfig import TG_TOKEN
+
+TG_CHAT_ID = "-1001537505605"
+bot = telegram.Bot(TG_TOKEN, base_url="http://0.0.0.0:8081/bot")
+
+bot.send_photo(TG_CHAT_ID, open('space.png', 'rb'))
+bot.send_document(TG_CHAT_ID, open('app-arm64-v8a-debug.apk', 'rb'))
+bot.send_document(TG_CHAT_ID, open('app-armeabi-v7a-debug.apk', 'rb'))
+bot.send_document(TG_CHAT_ID, open('app-x86_64-debug.apk', 'rb'))
+bot.send_document(TG_CHAT_ID, open('app-x86-debug.apk', 'rb'))
