8ac0a07832f1381ffb7b806fd4535f245abf6756
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-12 01:35:01 +0530

    Switch to Cloud API

--- .github/tg.py
+++ .github/tg.py
@@ -2,7 +2,7 @@ import telegram
 from tgconfig import TG_TOKEN
 
 TG_CHAT_ID = "-1001537505605"
-bot = telegram.Bot(TG_TOKEN, base_url="http://0.0.0.0:8081/bot")
+bot = telegram.Bot(TG_TOKEN)
 
 bot.send_photo(TG_CHAT_ID, open('space.png', 'rb'))
 bot.send_document(TG_CHAT_ID, open('app-arm64-v8a-debug.apk', 'rb'))
