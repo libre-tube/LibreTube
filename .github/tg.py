cc1c6bd8245f6f766f2d3598ef87eacb61cbf631
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-12 23:11:49 +0530

    New format

--- .github/tg.py
+++ .github/tg.py
@@ -1,10 +1,24 @@
 import telegram
 from tgconfig import TG_TOKEN
+from json import load
+from time import sleep
+
+f = open('commit.json')
+data = load(f)
+  
+title = f'''*Libretube {data['sha'][0:7]} // Alpha*
+
+{data['commit']['message']}
+
+Signed-off-by: {data['commit']['author']['name']} <{data['commit']['author']['email']}>
+'''
+  
+f.close()
 
 TG_CHAT_ID = "-1001537505605"
 bot = telegram.Bot(TG_TOKEN)
 
-bot.send_photo(TG_CHAT_ID, open('space.png', 'rb'))
+bot.send_photo(TG_CHAT_ID, open('alpha.png', 'rb'), title, telegram.ParseMode.MARKDOWN)
 bot.send_document(TG_CHAT_ID, open('app-arm64-v8a-debug.apk', 'rb'))
 bot.send_document(TG_CHAT_ID, open('app-armeabi-v7a-debug.apk', 'rb'))
 bot.send_document(TG_CHAT_ID, open('app-x86_64-debug.apk', 'rb'))
