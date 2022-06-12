372e46b7102c9e337e8424f5108dee3657b5e117
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-12 23:39:14 +0530

    Fix Caption

--- .github/tg.py
+++ .github/tg.py
@@ -9,12 +9,10 @@ data = load(f)
 TG_CHAT_ID = "-1001537505605"
 bot = telegram.Bot(TG_TOKEN)
 
-bot.send_photo(TG_CHAT_ID, open('alpha.png', 'rb'), f'''<b>Libretube {data['sha'][0:7]} // Alpha</b>
+bot.send_photo(TG_CHAT_ID, open('alpha.png', 'rb'), f'''Libretube {data['sha'][0:7]} // Alpha
 
 {data['commit']['message']}
-
-Signed-off-by: {data['commit']['author']['name']} <{data['commit']['author']['email']}>
-''', telegram.ParseMode.HTML)
+''')
 bot.send_document(TG_CHAT_ID, open('app-arm64-v8a-debug.apk', 'rb'))
 bot.send_document(TG_CHAT_ID, open('app-armeabi-v7a-debug.apk', 'rb'))
 bot.send_document(TG_CHAT_ID, open('app-x86_64-debug.apk', 'rb'))
