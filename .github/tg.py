a57ab1da793cfbc74300129376b9ac92141d2104
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-12 23:32:36 +0530

    Integrate Variable

--- .github/tg.py
+++ .github/tg.py
@@ -5,21 +5,19 @@ from time import sleep
 
 f = open('commit.json')
 data = load(f)
-  
-title = f'''<b>Libretube {data['sha'][0:7]} // Alpha</b>
-
-{data['commit']['message']}
-
-Signed-off-by: {data['commit']['author']['name']} <{data['commit']['author']['email']}>
-'''
-  
-f.close()
 
 TG_CHAT_ID = "-1001537505605"
 bot = telegram.Bot(TG_TOKEN)
 
-bot.send_photo(TG_CHAT_ID, open('alpha.png', 'rb'), title, telegram.ParseMode.HTML)
+bot.send_photo(TG_CHAT_ID, open('alpha.png', 'rb'), f'''<b>Libretube {data['sha'][0:7]} // Alpha</b>
+
+{data['commit']['message']}
+
+Signed-off-by: {data['commit']['author']['name']} <{data['commit']['author']['email']}>
+''', telegram.ParseMode.HTML)
 bot.send_document(TG_CHAT_ID, open('app-arm64-v8a-debug.apk', 'rb'))
 bot.send_document(TG_CHAT_ID, open('app-armeabi-v7a-debug.apk', 'rb'))
 bot.send_document(TG_CHAT_ID, open('app-x86_64-debug.apk', 'rb'))
 bot.send_document(TG_CHAT_ID, open('app-x86-debug.apk', 'rb'))
+
+f.close()
