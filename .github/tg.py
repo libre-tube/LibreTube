1218eb53d5602e810ac6f666f976243d033784e2
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-16 14:33:41 +0530

    Build Bot API

--- .github/tg.py
+++ .github/tg.py
@@ -1,21 +1,21 @@
 import telegram
-from tgconfig import TG_TOKEN
+from tgconfig import *
 from json import load
 
 f = open('commit.json')
 data = load(f)
 f.close()
 
-TG_CHAT_ID = "-1001537505605"
+TG_POST_ID = "-1001537505605"
 bot = telegram.Bot(TG_TOKEN)
 
-bot.send_photo(TG_CHAT_ID, open('alpha.png', 'rb'), f'''*Libretube {data['sha'][0:7]} // Alpha*
+bot.send_photo(TG_POST_ID, open('alpha.png', 'rb'), f'''*Libretube {data['sha'][0:7]} // Alpha*
 
 {data['commit']['message']}
 
 Signed-off-by: {data['commit']['author']['name']}
 ''', parse_mode=telegram.ParseMode.MARKDOWN)
-bot.send_document(TG_CHAT_ID, open('app-arm64-v8a-debug.apk', 'rb'))
-bot.send_document(TG_CHAT_ID, open('app-armeabi-v7a-debug.apk', 'rb'))
-bot.send_document(TG_CHAT_ID, open('app-x86_64-debug.apk', 'rb'))
-bot.send_document(TG_CHAT_ID, open('app-x86-debug.apk', 'rb'))
+bot.send_document(TG_POST_ID, open('app-arm64-v8a-debug.apk', 'rb'))
+bot.send_document(TG_POST_ID, open('app-armeabi-v7a-debug.apk', 'rb'))
+bot.send_document(TG_POST_ID, open('app-x86_64-debug.apk', 'rb'))
+bot.send_document(TG_POST_ID, open('app-x86-debug.apk', 'rb'))
