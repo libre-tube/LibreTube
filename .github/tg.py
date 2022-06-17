a4fe1526f468cab28aa0d4c16c50d0b2c84a5e12
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-18 00:47:55 +0530

    Fix Variable

--- .github/tg.py
+++ .github/tg.py
@@ -21,7 +21,7 @@ def bot():
 
 Signed-off-by: {data['commit']['author']['name']}
 ''', parse_mode=telegram.ParseMode.MARKDOWN)
-    bot.send_media_group(TG_CHAT_ID, [telegram.InputMediaDocument(open('app-x86-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-x86_64-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-armeabi-v7a-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-arm64-v8a-debug.apk', 'rb'))])
+    bot.send_media_group(TG_POST_ID, [telegram.InputMediaDocument(open('app-x86-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-x86_64-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-armeabi-v7a-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-arm64-v8a-debug.apk', 'rb'))])
     run('pid=$(pgrep bot-api)')
     run('pkill $pid')
     
