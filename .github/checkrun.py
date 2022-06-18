cb8708d646026e07a4f1048793b5f7059804b814
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-18 20:34:35 +0530

    Check for Execution order

---
+++ .github/checkrun.py
@@ -0,0 +1,12 @@
+from json import load
+from os import system
+
+f = open('commit.json')
+data = load(f)
+f.close()
+
+message = data['commit']['message']
+
+if "\n\n" in message:
+    if message.split("\n\n",1)[-1].split()[0] == "[SILENT]":
+        system('killall -9 python')
--- .github/tg.py
+++ .github/tg.py
@@ -2,11 +2,11 @@ import telegram
 from tgconfig import *
 from json import load
 import multiprocessing
-import os
+from os import system
 from time import sleep as wait
 
 def deploy():
-    os.system(f'~/bot-api --local --api-id={TG_API_ID} --api-hash={TG_API_HASH}')
+    system(f'~/bot-api --local --api-id={TG_API_ID} --api-hash={TG_API_HASH}')
 
 def bot():
     wait(10)
@@ -22,7 +22,7 @@ def bot():
 Signed-off-by: {data['commit']['author']['name']}
 ''', parse_mode=telegram.ParseMode.MARKDOWN)
     bot.send_media_group(TG_POST_ID, [telegram.InputMediaDocument(open('app-x86-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-x86_64-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-armeabi-v7a-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-arm64-v8a-debug.apk', 'rb'))])
-    os.system('killall -9 python || true')
+    system('killall -9 python')
     
 if __name__ == '__main__':
     multideploy = multiprocessing.Process(target=deploy)
