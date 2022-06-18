eab7a8d9ba5ec33f7f9cf5dcf1f9de29d33d581e
XelXen <78258352+XelXen@users.noreply.github.com>
2022-06-18 21:13:46 +0530

    Add link to commit

--- .github/tg.py
+++ .github/tg.py
@@ -17,11 +17,10 @@ def bot():
     bot = telegram.Bot(TG_TOKEN, base_url="http://0.0.0.0:8081/bot")
     bot.send_photo(TG_POST_ID, open('alpha.png', 'rb'), f'''*Libretube {data['sha'][0:7]} // Alpha*
 
-{data['commit']['message']}
+[{data['commit']['message']}]({data['commit']['url']})
 
 Signed-off-by: {data['commit']['author']['name']}
-[inline URL](http://www.example.com/)
-''', parse_mode="MarkdownV2")
+''', parse_mode=telegram.ParseMode.MARKDOWN)
     bot.send_media_group(TG_POST_ID, [telegram.InputMediaDocument(open('app-x86-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-x86_64-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-armeabi-v7a-debug.apk', 'rb')), telegram.InputMediaDocument(open('app-arm64-v8a-debug.apk', 'rb'))])
     system('killall -9 python')
     
