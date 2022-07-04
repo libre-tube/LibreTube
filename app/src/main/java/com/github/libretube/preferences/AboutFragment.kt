e8dfbbc7f68115a5c97b9c58a5d07e0568862290
Bnyro <yannick.fehler@web.de>
2022-07-04 18:09:20 +0200

    fix line count in snackbar

--- app/src/main/java/com/github/libretube/preferences/AboutFragment.kt
+++ app/src/main/java/com/github/libretube/preferences/AboutFragment.kt
@@ -93,8 +93,14 @@ class AboutFragment : Fragment() {
     private fun showSnackBar(text: String) {
         val snackBar = Snackbar
             .make(binding.root, text, Snackbar.LENGTH_LONG)
+
+        // set snackBar color
         snackBar.setBackgroundTint(getThemeColor(requireContext(), R.attr.colorSurface))
         snackBar.setTextColor(getThemeColor(requireContext(), R.attr.colorPrimary))
+
+        // prevent the text from being partially hidden
+        snackBar.setTextMaxLines(3)
+
         snackBar.show()
     }
 
