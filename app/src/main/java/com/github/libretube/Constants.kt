05c4742facdf7bafff3087b165e18aab8811dc53
Bnyro <yannick.fehler@web.de>
2022-07-05 11:14:51 +0200

    add community section

--- app/src/main/java/com/github/libretube/Constants.kt
+++ app/src/main/java/com/github/libretube/Constants.kt
@@ -1,7 +1,22 @@
-package com.github.libretube.util
+package com.github.libretube
 
+/**
+ * API link for the update checker
+ */
 const val GITHUB_API_URL = "https://api.github.com/repos/libre-tube/LibreTube/releases/latest"
+
+/**
+ * Links for the about fragment
+ */
 const val WEBSITE_URL = "https://libre-tube.github.io/"
 const val DONATE_URL = "https://github.com/libre-tube/LibreTube#donate"
 const val GITHUB_URL = "https://github.com/libre-tube/LibreTube"
 const val PIPED_GITHUB_URL = "https://github.com/TeamPiped/Piped"
+
+/**
+ * Social media links for the community fragment
+ */
+const val TELEGRAM_URL = ""
+const val MATRIX_URL = ""
+const val DISCORD_URL = ""
+const val REDDIT_URL = ""
---
+++ app/src/main/java/com/github/libretube/preferences/CommunityFragment.kt
@@ -0,0 +1,58 @@
+package com.github.libretube.preferences
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.libretube.DISCORD_URL
+import com.github.libretube.MATRIX_URL
+import com.github.libretube.R
+import com.github.libretube.REDDIT_URL
+import com.github.libretube.TELEGRAM_URL
+import com.github.libretube.activities.SettingsActivity
+import com.github.libretube.databinding.FragmentCommunityBinding
+
+class CommunityFragment : Fragment() {
+    private lateinit var binding: FragmentCommunityBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCommunityBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val settingsActivity = activity as SettingsActivity
+        settingsActivity.changeTopBarText(getString(R.string.community))
+
+        binding.telegram.setOnClickListener {
+            openLinkFromHref(TELEGRAM_URL)
+        }
+
+        binding.matrix.setOnClickListener {
+            openLinkFromHref(MATRIX_URL)
+        }
+
+        binding.discord.setOnClickListener {
+            openLinkFromHref(DISCORD_URL)
+        }
+
+        binding.reddit.setOnClickListener {
+            openLinkFromHref(REDDIT_URL)
+        }
+    }
+
+    private fun openLinkFromHref(link: String) {
+        val uri = Uri.parse(link)
+        val intent = Intent(Intent.ACTION_VIEW).setData(uri)
+        startActivity(intent)
+    }
+}
