c199d42116a2f9aa3a96c2de1445e91da3168ddf
Bnyro <yannick.fehler@web.de>
2022-07-07 22:30:47 +0200

    attempt to fix auto regions

--- app/src/main/java/com/github/libretube/util/LocaleHelper.kt
+++ app/src/main/java/com/github/libretube/util/LocaleHelper.kt
@@ -2,6 +2,7 @@ package com.github.libretube.util
 
 import android.content.Context
 import android.os.Build
+import android.telephony.TelephonyManager
 import com.github.libretube.preferences.PreferenceHelper
 import java.util.*
 
@@ -36,5 +37,51 @@ object LocaleHelper {
             conf.locale = locale
         }
         res.updateConfiguration(conf, dm)
+    }
+
+    fun getDetectedCountry(context: Context, defaultCountryIsoCode: String): String {
+
+        detectSIMCountry(context)?.let {
+            return it
+        }
+
+        detectNetworkCountry(context)?.let {
+            return it
+        }
+
+        detectLocaleCountry(context)?.let {
+            return it
+        }
+
+        return defaultCountryIsoCode
+    }
+
+    private fun detectSIMCountry(context: Context): String? {
+        try {
+            val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            return telephonyManager.simCountryIso
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    private fun detectNetworkCountry(context: Context): String? {
+        try {
+            val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            return telephonyManager.networkCountryIso
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    private fun detectLocaleCountry(context: Context): String? {
+        try {
+            return context.resources.configuration.locales[0].country
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
     }
 }
