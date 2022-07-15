ae08491d0bebad08206908df883f486fc911dede
Bnyro <yannick.fehler@web.de>
2022-07-15 11:27:04 +0200

    add toggable options menu

--- app/src/main/java/com/github/libretube/views/CustomExoPlayerView.kt
+++ app/src/main/java/com/github/libretube/views/CustomExoPlayerView.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.View
 import com.github.libretube.databinding.ExoStyledPlayerControlViewBinding
 import com.google.android.exoplayer2.ui.StyledPlayerView
 
@@ -12,6 +13,12 @@ internal class CustomExoPlayerView(
     attributeSet: AttributeSet? = null
 ) : StyledPlayerView(context, attributeSet) {
     val binding: ExoStyledPlayerControlViewBinding = ExoStyledPlayerControlViewBinding.bind(this)
+
+    override fun hideController() {
+        super.hideController()
+        binding.toggleOptions.animate().rotationX(0F).start()
+        binding.advancedOptions.visibility = View.GONE
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
