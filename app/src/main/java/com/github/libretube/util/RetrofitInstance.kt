1f78d4d2c51fc2c63ab3650d3a008d6640a93e1c
Kavin <20838718+FireMasterK@users.noreply.github.com>
2022-07-21 19:06:15 +0530

    Use cronet call factory for api requests.

--- app/src/main/java/com/github/libretube/util/RetrofitInstance.kt
+++ app/src/main/java/com/github/libretube/util/RetrofitInstance.kt
@@ -1,5 +1,6 @@
 package com.github.libretube.util
 
+import com.google.net.cronet.okhttptransport.CronetCallFactory
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 
@@ -7,9 +8,13 @@ object RetrofitInstance {
     lateinit var url: String
     lateinit var authUrl: String
     val lazyMgr = resettableManager()
+    private val callFactory: CronetCallFactory =
+        CronetCallFactory.newBuilder(CronetHelper.getCronetEngine())
+            .build()
     val api: PipedApi by resettableLazy(lazyMgr) {
         Retrofit.Builder()
             .baseUrl(url)
+            .callFactory(callFactory)
             .addConverterFactory(JacksonConverterFactory.create())
             .build()
             .create(PipedApi::class.java)
@@ -17,6 +22,7 @@ object RetrofitInstance {
     val authApi: PipedApi by resettableLazy(lazyMgr) {
         Retrofit.Builder()
             .baseUrl(authUrl)
+            .callFactory(callFactory)
             .addConverterFactory(JacksonConverterFactory.create())
             .build()
             .create(PipedApi::class.java)
