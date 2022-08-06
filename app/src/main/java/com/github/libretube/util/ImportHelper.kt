3b19d9b99575b35e7903f9c2ca38754185c04ed4
Bnyro <yannick.fehler@web.de>
2022-08-06 12:51:53 +0200

    .

--- app/src/main/java/com/github/libretube/util/ImportHelper.kt
+++ app/src/main/java/com/github/libretube/util/ImportHelper.kt
@@ -110,7 +110,7 @@ class ImportHelper(
                     newPipeChannels += NewPipeSubscription(
                         name = it.name,
                         service_id = 0,
-                        url = "https://youtube.com" + it.url
+                        url = "https://www.youtube.com" + it.url
                     )
                 }
 
