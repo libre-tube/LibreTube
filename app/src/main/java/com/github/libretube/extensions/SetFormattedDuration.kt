1e54f20d0331d73f73d3e62671bc87c34ada87bc
Bnyro <yannick.fehler@web.de>
2022-09-11 17:17:36 +0200

    fix inconsistent label

--- app/src/main/java/com/github/libretube/extensions/SetFormattedDuration.kt
+++ app/src/main/java/com/github/libretube/extensions/SetFormattedDuration.kt
@@ -7,20 +7,8 @@ import com.github.libretube.util.ThemeHelper
 
 fun TextView.setFormattedDuration(duration: Long) {
     val text = if (duration < 0L) {
-        this.setBackgroundColor(
-            ThemeHelper.getThemeColor(
-                context,
-                R.attr.colorPrimaryDark
-            )
-        )
         this.context.getString(R.string.live)
     } else if (duration in 0L..60L) {
-        this.setBackgroundColor(
-            ThemeHelper.getThemeColor(
-                context,
-                R.attr.colorPrimaryDark
-            )
-        )
         this.context.getString(R.string.yt_shorts)
     } else {
         DateUtils.formatElapsedTime(duration)
