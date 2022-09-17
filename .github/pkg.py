ecd41f7db3837267abc8c186d5ff5eef14df72a2
XelXen <78258352+XelXen@users.noreply.github.com>
2022-09-17 23:37:44 +0530

    Fix Collecting Shell Output

--- .github/pkg.py
+++ .github/pkg.py
@@ -1,25 +1,29 @@
-import subprocess
+import os
 
 while True:
-    outputStr = subprocess.call("curl https://raw.githubusercontent.com/LibreTubeAlpha/Archive/main/latestBuild/app-x86-debug.apk --output ~/app-x86-debug.apk", shell=True)
+    os.system("curl https://raw.githubusercontent.com/LibreTubeAlpha/Archive/main/latestBuild/app-x86-debug.apk --output app-x86-debug.apk")
+    outputStr = open("app-x86-debug.apk","r").read()
     if "Not Found" not in outputStr: 
         print("Build [x86] Downloaded")
         break
 
 while True:
-    outputStr = subprocess.call("curl https://raw.githubusercontent.com/LibreTubeAlpha/Archive/main/latestBuild/app-x86_64-debug.apk --output ~/app-x86_64-debug.apk", shell=True)
+    os.system("curl https://raw.githubusercontent.com/LibreTubeAlpha/Archive/main/latestBuild/app-x86_64-debug.apk --output app-x86_64-debug.apk")
+    outputStr = open("app-x86_64-debug.apk","r").read()
     if "Not Found" not in outputStr: 
         print("Build [x86_64] Downloaded")
         break
 
 while True:
-    outputStr = subprocess.call("curl https://raw.githubusercontent.com/LibreTubeAlpha/Archive/main/latestBuild/app-armeabi-v7a-debug.apk --output ~/app-armeabi-v7a-debug.apk", shell=True)
+    os.system("curl https://raw.githubusercontent.com/LibreTubeAlpha/Archive/main/latestBuild/app-armeabi-v7a-debug.apk --output app-armeabi-v7a-debug.apk")
+    outputStr = open("app-armeabi-v7a-debug.apk","r").read()
     if "Not Found" not in outputStr: 
         print("Build [arm7] Downloaded")
         break
 
 while True:
-    outputStr = subprocess.call("curl https://raw.githubusercontent.com/LibreTubeAlpha/Archive/main/latestBuild/app-armeabi-v8a-debug.apk --output ~/app-armeabi-v8a-debug.apk", shell=True)
+    os.system("curl https://raw.githubusercontent.com/LibreTubeAlpha/Archive/main/latestBuild/app-armeabi-v8a-debug.apk --output app-armeabi-v8a-debug.apk")
+    outputStr = open("app-armeabi-v8a-debug.apk","r").read()
     if "Not Found" not in outputStr: 
         print("Build [arm8] Downloaded")
         break
