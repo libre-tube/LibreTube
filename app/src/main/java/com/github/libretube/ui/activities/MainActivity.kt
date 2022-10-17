c9c34a090deb35fe300700bf2e7dfbc98d18f9b2
Bnyro <bnyro@tutanota.com>
2022-10-17 19:18:48 +0200

    ktlint

--- app/src/main/java/com/github/libretube/ui/activities/MainActivity.kt
+++ app/src/main/java/com/github/libretube/ui/activities/MainActivity.kt
@@ -8,7 +8,6 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
