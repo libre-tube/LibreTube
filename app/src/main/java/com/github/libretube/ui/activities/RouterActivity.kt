dc261478c6a67c9f6cc72b0a4f8c32ea169d7c42
Bnyro <bnyro@tutanota.com>
2022-10-18 17:20:21 +0200

    fix the share behavior

--- app/src/main/java/com/github/libretube/ui/activities/RouterActivity.kt
+++ app/src/main/java/com/github/libretube/ui/activities/RouterActivity.kt
@@ -74,13 +74,13 @@ class RouterActivity : BaseActivity() {
                 val videoId = uri.getQueryParameter("v")
 
                 intent.putExtra(IntentData.videoId, videoId)
+                uri.getQueryParameter("t")?.let { intent.putExtra(IntentData.timeStamp, it.toLong()) }
             }
             else -> {
-                val timeStamp = uri.getQueryParameter("t")
                 val videoId = uri.path!!.replace("/", "")
 
                 intent.putExtra(IntentData.videoId, videoId)
-                if (timeStamp != null) intent.putExtra(IntentData.timeStamp, timeStamp.toLong())
+                uri.getQueryParameter("t")?.let { intent.putExtra(IntentData.timeStamp, it.toLong()) }
             }
         }
         return intent
