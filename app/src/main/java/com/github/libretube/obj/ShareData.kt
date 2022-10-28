433a929ac34aedb15a950a960ca549032901a7ca
shantanu1k <shantanukawade2@gmail.com>
2022-10-28 20:02:50 +0530

    New class to share Video/Channel/Playlist name

---
+++ app/src/main/java/com/github/libretube/obj/ShareData.kt
@@ -0,0 +1,7 @@
+package com.github.libretube.obj
+
+data class ShareData(
+    val currentChannel: String ? = null,
+    val currentVideo: String ? = null,
+    val currentPlaylist: String ? = null
+)
