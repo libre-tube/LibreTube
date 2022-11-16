74651b761709a5295f65ed18a96feb01bdb75953
Bnyro <bnyro@tutanota.com>
2022-11-16 18:42:57 +0100

    option to force hls

--- app/src/main/java/com/github/libretube/constants/PreferenceKeys.kt
+++ app/src/main/java/com/github/libretube/constants/PreferenceKeys.kt
@@ -81,6 +81,7 @@ object PreferenceKeys {
     const val LIMIT_HLS = "limit_hls"
     const val PROGRESSIVE_LOADING_INTERVAL_SIZE = "progressive_loading_interval"
     const val ALTERNATIVE_PLAYER_LAYOUT = "alternative_player_layout"
+    const val USE_HLS_OVER_DASH = "use_hls"
 
     /**
      * Background mode
