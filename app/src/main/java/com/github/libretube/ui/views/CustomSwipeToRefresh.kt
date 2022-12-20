b607ea945bd2e16ae39eb94620112e737dfbed91
Bnyro <bnyro@tutanota.com>
2022-12-20 16:37:37 +0100

    Theme the pull refresh progress

--- app/src/main/java/com/github/libretube/ui/views/CustomSwipeToRefresh.kt
+++ app/src/main/java/com/github/libretube/ui/views/CustomSwipeToRefresh.kt
@@ -1,31 +1,38 @@
 package com.github.libretube.ui.views
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.MotionEvent.ACTION_MOVE
 import android.view.ViewConfiguration
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.github.libretube.R
+import com.github.libretube.util.ThemeHelper
+import kotlin.math.abs
 
 class CustomSwipeToRefresh(context: Context?, attrs: AttributeSet?) :
     SwipeRefreshLayout(context!!, attrs) {
-    private val mTouchSlop: Int
+    private val mTouchSlop: Int = ViewConfiguration.get(this.context).scaledTouchSlop
     private var mPrevX = 0f
+
+    init {
+        setColorSchemeColors(ThemeHelper.getThemeColor(this.context, R.attr.colorPrimary))
+        setProgressBackgroundColorSchemeColor(ThemeHelper.getThemeColor(this.context, R.attr.colorSurface))
+    }
+
+    @SuppressLint("Recycle")
     override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> mPrevX = MotionEvent.obtain(event).x
             ACTION_MOVE -> {
                 val eventX = event.x
-                val xDiff = Math.abs(eventX - mPrevX)
+                val xDiff = abs(eventX - mPrevX)
                 if (xDiff > mTouchSlop) {
                     return false
                 }
             }
         }
         return super.onInterceptTouchEvent(event)
-    }
-
-    init {
-        mTouchSlop = ViewConfiguration.get(this.context).scaledTouchSlop
     }
 }
