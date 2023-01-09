3dd58d47fad84c5fe7ed1b6461860cb932ffda8c
Bnyro <bnyro@tutanota.com>
2023-01-09 17:38:17 +0100

    Option for background and skip controls in PiP

--- app/src/main/java/com/github/libretube/enums/PlayerEvent.kt
+++ app/src/main/java/com/github/libretube/enums/PlayerEvent.kt
@@ -6,7 +6,8 @@ enum class PlayerEvent(val value: Int) {
     Forward(2),
     Rewind(3),
     Next(5),
-    Prev(6);
+    Prev(6),
+    Background(7);
 
     companion object {
         fun fromInt(value: Int) = PlayerEvent.values().first { it.value == value }
