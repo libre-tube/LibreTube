da0405935968a20bb32a8283b502c99dd7afd243
Bnyro <bnyro@tutanota.com>
2023-02-18 10:29:26 +0100

    Better error messages when a video is unavailable

--- app/src/main/java/com/github/libretube/api/obj/Message.kt
+++ app/src/main/java/com/github/libretube/api/obj/Message.kt
@@ -3,4 +3,7 @@ package com.github.libretube.api.obj
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Message(val message: String? = null)
+data class Message(
+    val error: String? = null,
+    val message: String? = null
+)
