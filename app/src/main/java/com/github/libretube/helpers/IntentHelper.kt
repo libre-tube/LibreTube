e08505491f3ceef8a913611bb73e70b512939a77
Bnyro <82752168+Bnyro@users.noreply.github.com>
2023-03-10 18:00:42 +0100

    Fix crash when clicking links in about without browser available (#3284)
    * Fix crash when clicking links in about without browser available

--- app/src/main/java/com/github/libretube/helpers/IntentHelper.kt
+++ app/src/main/java/com/github/libretube/helpers/IntentHelper.kt
@@ -3,11 +3,17 @@ package com.github.libretube.helpers
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import com.github.libretube.R
+import com.github.libretube.extensions.toastFromMainThread
 
 object IntentHelper {
     fun openLinkFromHref(context: Context, link: String) {
         val uri = Uri.parse(link)
         val launchIntent = Intent(Intent.ACTION_VIEW).setData(uri)
-        context.startActivity(launchIntent)
+        try {
+            context.startActivity(launchIntent)
+        } catch (e: Exception) {
+            context.toastFromMainThread(R.string.unknown_error)
+        }
     }
 }
