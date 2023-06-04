2573c3732220f263bf8bb50d65b09c89c5e878b2
Bnyro <bnyro@tutanota.com>
2023-06-04 16:59:25 +0200

    Show 'mark as unwatched' in video options sheet if already watched

--- app/src/main/java/com/github/libretube/db/dao/WatchHistoryDao.kt
+++ app/src/main/java/com/github/libretube/db/dao/WatchHistoryDao.kt
@@ -6,11 +6,15 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.github.libretube.db.obj.WatchHistoryItem
+import com.github.libretube.db.obj.WatchPosition
 
 @Dao
 interface WatchHistoryDao {
     @Query("SELECT * FROM watchHistoryItem")
     suspend fun getAll(): List<WatchHistoryItem>
+
+    @Query("SELECT * FROM watchHistoryItem WHERE videoId LIKE :videoId LIMIT 1")
+    suspend fun findById(videoId: String): WatchHistoryItem?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(watchHistoryItem: WatchHistoryItem)
@@ -20,6 +24,9 @@ interface WatchHistoryDao {
 
     @Delete
     suspend fun delete(watchHistoryItem: WatchHistoryItem)
+
+    @Query("DELETE FROM watchHistoryItem WHERE videoId = :id")
+    fun deleteByVideoId(id: String)
 
     @Query("DELETE FROM watchHistoryItem")
     suspend fun deleteAll()
