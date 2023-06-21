f472713bf11fa5da509bd5375ac581974b40ce62
general-a <404aaronm@gmail.com>
2023-06-20 19:54:34 -0500

    Removed manual toast that was added in this pr

--- app/src/main/java/com/github/libretube/ui/fragments/PlayerFragment.kt
+++ app/src/main/java/com/github/libretube/ui/fragments/PlayerFragment.kt
@@ -669,7 +669,6 @@ class PlayerFragment : Fragment(), OnlinePlayerOptions {
                 binding.sbSkipBtn.setOnClickListener {
                     exoPlayer.seekTo(segmentEnd)
                     binding.sbSkipBtn.visibility = View.GONE
-                    Toast.makeText(context, R.string.segment_skipped, Toast.LENGTH_SHORT).show()
                 }
                 return
             }
