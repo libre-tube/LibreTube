071e6311a1d5b291361ff154553e4437dd033141
Bnyro <bnyro@tutanota.com>
2023-07-19 11:04:59 +0200

    feat: swipe to delete downloads

--- app/src/main/java/com/github/libretube/ui/sheets/DownloadOptionsBottomSheet.kt
+++ app/src/main/java/com/github/libretube/ui/sheets/DownloadOptionsBottomSheet.kt
@@ -5,22 +5,16 @@ import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.github.libretube.R
 import com.github.libretube.constants.IntentData
-import com.github.libretube.db.DatabaseHolder
 import com.github.libretube.db.obj.Download
-import com.github.libretube.db.obj.DownloadItem
 import com.github.libretube.enums.ShareObjectType
 import com.github.libretube.helpers.NavigationHelper
 import com.github.libretube.obj.ShareData
 import com.github.libretube.services.OfflinePlayerService
 import com.github.libretube.ui.dialogs.ShareDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlin.io.path.deleteIfExists
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 
 class DownloadOptionsBottomSheet(
     private val download: Download,
-    private val items: List<DownloadItem>,
     private val onDelete: () -> Unit
 ) : BaseBottomSheet() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,23 +44,8 @@ class DownloadOptionsBottomSheet(
                 }
 
                 3 -> {
-                    MaterialAlertDialogBuilder(requireContext())
-                        .setTitle(R.string.delete)
-                        .setMessage(R.string.irreversible)
-                        .setPositiveButton(R.string.okay) { _, _ ->
-                            items.forEach {
-                                it.path.deleteIfExists()
-                            }
-                            download.thumbnailPath?.deleteIfExists()
-
-                            runBlocking(Dispatchers.IO) {
-                                DatabaseHolder.Database.downloadDao().deleteDownload(download)
-                            }
-                            onDelete.invoke()
-                            dialog?.dismiss()
-                        }
-                        .setNegativeButton(R.string.cancel, null)
-                        .show()
+                    onDelete.invoke()
+                    dialog?.dismiss()
                 }
             }
         }
