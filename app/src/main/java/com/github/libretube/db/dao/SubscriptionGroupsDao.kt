976d5d36c096295005b82bf8f045628c658f56f7
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-08-20 16:19:34 +0200

    feat(channelGroups): allow re-ordering of groups (#4551)

--- app/src/main/java/com/github/libretube/db/dao/SubscriptionGroupsDao.kt
+++ app/src/main/java/com/github/libretube/db/dao/SubscriptionGroupsDao.kt
@@ -4,11 +4,12 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.github.libretube.db.obj.SubscriptionGroup
 
 @Dao()
 interface SubscriptionGroupsDao {
-    @Query("SELECT * FROM subscriptionGroups")
+    @Query("SELECT * FROM subscriptionGroups ORDER BY `index` ASC")
     suspend fun getAll(): List<SubscriptionGroup>
 
     @Query("SELECT EXISTS(SELECT * FROM subscriptionGroups WHERE name = :name)")
@@ -19,6 +20,9 @@ interface SubscriptionGroupsDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(subscriptionGroups: List<SubscriptionGroup>)
+
+    @Update
+    suspend fun updateAll(subscriptionGroups: List<SubscriptionGroup>)
 
     @Query("DELETE FROM subscriptionGroups WHERE name = :name")
     suspend fun deleteGroup(name: String)
--- app/src/main/java/com/github/libretube/db/obj/SubscriptionGroup.kt
+++ app/src/main/java/com/github/libretube/db/obj/SubscriptionGroup.kt
@@ -8,5 +8,6 @@ import kotlinx.serialization.Serializable
 @Entity(tableName = "subscriptionGroups")
 data class SubscriptionGroup(
     @PrimaryKey var name: String,
-    var channels: List<String>
+    var channels: List<String>,
+    var index: Int
 )
