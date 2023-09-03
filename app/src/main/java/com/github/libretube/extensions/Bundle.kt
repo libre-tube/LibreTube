418d7a441237b98e76f5cdb65f1b841323ee6d43
Isira Seneviratne <isirasen96@gmail.com>
2023-09-04 04:52:23 +0530

    refactor: Combine Bundle extensions to one file

--- app/src/main/java/com/github/libretube/extensions/Bundle.kt
+++ app/src/main/java/com/github/libretube/extensions/Bundle.kt
@@ -2,8 +2,21 @@ package com.github.libretube.extensions
 
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.annotation.OptIn
+import androidx.core.os.BuildCompat
 import androidx.core.os.BundleCompat
+import java.io.Serializable
 
 inline fun <reified T : Parcelable> Bundle.parcelable(key: String?): T? {
     return BundleCompat.getParcelable(this, key, T::class.java)
+}
+
+@OptIn(BuildCompat.PrereleaseSdkCheck::class)
+inline fun <reified T : Serializable> Bundle.serializable(key: String): T? {
+    return if (BuildCompat.isAtLeastU()) {
+        getSerializable(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getSerializable(key) as? T
+    }
 }
