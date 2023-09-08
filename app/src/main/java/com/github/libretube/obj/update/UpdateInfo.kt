4f1375a63d730bc907b905b61838588a860808b6
Isira Seneviratne <isirasen96@gmail.com>
2023-09-08 20:17:41 +0530

    refactor: Implement Parcelable in UpdateInfo

--- app/src/main/java/com/github/libretube/obj/update/UpdateInfo.kt
+++ app/src/main/java/com/github/libretube/obj/update/UpdateInfo.kt
@@ -1,10 +1,13 @@
 package com.github.libretube.obj.update
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Parcelize
 data class UpdateInfo(
     @SerialName("html_url") val htmlUrl: String,
     val name: String
-)
+) : Parcelable
