8ea8e86f78d731e1ba6828882e1efd3c4e663f4c
IndusAryan <serena01011975@gmail.com>
2023-09-16 18:28:10 +0530

    bump ksp and refactor libs catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 }
 
 plugins {
-    id("com.google.devtools.ksp") version("1.9.0-1.0.12") apply false
+    id("com.google.devtools.ksp") version("1.9.0-1.0.13") apply false
 }
 
 tasks.register<Delete>("clean") {
