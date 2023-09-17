5332f16fd2cd1f5f7740c7d8e526f9b6ab69dea1
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2023-09-17 20:00:03 +0530

    refactor: libs catalog and bump ksp (#4790)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
