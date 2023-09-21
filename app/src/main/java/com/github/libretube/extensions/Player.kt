libre-tube/LibreTube

commit 42d081f04e132bc4ff5dbf408c596bd1f6927992

fix(player): allow pausing while loading
Fixes https://github.com/libre-tube/LibreTube/issues/4784, the player should only pause when it has no buffer to play, not when it is loading, as it always loads, until the allocated buffer is full.

app/src/main/java/com/github/libretube/extensions/Player.kt
@@ -13,7 +13,7 @@ fun Player.togglePlayPauseState() {
             seekTo(0)
         }
 
-        !isPlaying && !isLoading -> play()
+        !isPlaying && totalBufferedDuration > 0 -> play()
         else -> pause()
     }
 }

commit adb051e24fe7cc68a22f3e4162893f839d3a8faf

fix: allow pausing when buffer is less than min buffer
Fixes a regression in 42d081f04e132bc4ff5dbf408c596bd1f6927992. The player should pause when it is unable to play, as long as the buffered time is less than the minimum buffer needed for playing.

app/src/main/java/com/github/libretube/extensions/Player.kt
@@ -1,6 +1,7 @@
 package com.github.libretube.extensions
 
 import androidx.media3.common.Player
+import com.github.libretube.helpers.PlayerHelper
 
 fun Player.togglePlayPauseState() {
     when {
@@ -13,7 +14,7 @@ fun Player.togglePlayPauseState() {
             seekTo(0)
         }
 
-        !isPlaying && totalBufferedDuration > 0 -> play()
+        !isPlaying && totalBufferedDuration > PlayerHelper.MINIMUM_BUFFER_DURATION -> play()
         else -> pause()
     }
 }

app/src/main/java/com/github/libretube/helpers/PlayerHelper.kt
@@ -46,6 +46,7 @@ object PlayerHelper {
     const val CONTROL_TYPE = "control_type"
     const val SPONSOR_HIGHLIGHT_CATEGORY = "poi_highlight"
     const val ROLE_FLAG_AUTO_GEN_SUBTITLE = C.ROLE_FLAG_SUPPLEMENTARY
+    const val MINIMUM_BUFFER_DURATION =  1000 * 10 // exo default is 50s
 
     /**
      * A list of all categories that are not disabled by default
@@ -437,7 +438,7 @@ object PlayerHelper {
             // cache the last three minutes
             .setBackBuffer(1000 * 60 * 3, true)
             .setBufferDurationsMs(
-                1000 * 10, // exo default is 50s
+                MINIMUM_BUFFER_DURATION,
                 bufferingGoal,
                 DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS,
                 DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS