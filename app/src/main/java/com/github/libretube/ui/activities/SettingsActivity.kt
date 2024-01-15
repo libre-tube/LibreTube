020a03a58c56fbbdb66aed5673d93f728f8c9a61
RafaRamos <rafaelsramos95@gmail.com>
2024-01-15 12:29:55 +0100

    Added support for redirecting directly to IntentSettings

--- app/src/main/java/com/github/libretube/ui/activities/SettingsActivity.kt
+++ app/src/main/java/com/github/libretube/ui/activities/SettingsActivity.kt
@@ -2,11 +2,13 @@ package com.github.libretube.ui.activities
 
 import android.os.Bundle
 import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.github.libretube.R
 import com.github.libretube.databinding.ActivitySettingsBinding
 import com.github.libretube.ui.base.BaseActivity
+import com.github.libretube.ui.preferences.InstanceSettings
 import com.github.libretube.ui.preferences.MainSettings
 
 class SettingsActivity : BaseActivity() {
@@ -24,9 +26,7 @@ class SettingsActivity : BaseActivity() {
         }
 
         if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                replace<MainSettings>(R.id.settings)
-            }
+            redirectTo<MainSettings>()
         }
 
         // new way of dealing with back presses instead of onBackPressed()
@@ -34,15 +34,35 @@ class SettingsActivity : BaseActivity() {
             if (supportFragmentManager.findFragmentById(R.id.settings) is MainSettings) {
                 finishAndRemoveTask()
             } else {
-                supportFragmentManager.commit {
-                    replace<MainSettings>(R.id.settings)
-                }
+                redirectTo<MainSettings>()
                 changeTopBarText(getString(R.string.settings))
             }
+        }
+
+        handleRedirect()
+    }
+
+    private fun handleRedirect() {
+        val redirectKey = intent.extras?.getString(REDIRECT_KEY)
+
+        when (redirectKey) {
+            REDIRECT_TO_INTENT_SETTINGS -> redirectTo<InstanceSettings>()
+            else                        -> { }
         }
     }
 
     fun changeTopBarText(text: String) {
         if (this::binding.isInitialized) binding.toolbar.title = text
+    }
+
+    private inline fun <reified T : Fragment> redirectTo() {
+        supportFragmentManager.commit {
+            replace<T>(R.id.settings)
+        }
+    }
+
+    companion object {
+        const val REDIRECT_KEY = "redirect"
+        const val REDIRECT_TO_INTENT_SETTINGS = "intent_settings"
     }
 }
