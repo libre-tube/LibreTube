e9e5a76304eb3615cf5a9eb11cec7e95f8d275df
Steffen Winter <steffen.winter@proton.me>
2024-03-11 22:39:19 +0100

    chore(deps): bump ksp to match kotlin 1.9.23

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
 }
 
 plugins {
-    id("com.google.devtools.ksp") version("1.9.22-1.0.17") apply false
+    id("com.google.devtools.ksp") version("1.9.23-1.0.19") apply false
     alias(libs.plugins.androidTest) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.baselineprofile) apply false
