f3049b51182639ca1107d0271280b36ad0ad4b5b
Bnyro <bnyro@tutanota.com>
2024-05-18 20:37:13 +0200

    fix: download activity not dismissed when dialog closed

--- app/src/main/java/com/github/libretube/ui/activities/DownloadActivity.kt
+++ app/src/main/java/com/github/libretube/ui/activities/DownloadActivity.kt
@@ -2,18 +2,16 @@ package com.github.libretube.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import androidx.core.os.bundleOf
 import com.github.libretube.constants.IntentData
 import com.github.libretube.enums.PlaylistType
 import com.github.libretube.helpers.IntentHelper
-import com.github.libretube.helpers.ThemeHelper
 import com.github.libretube.ui.base.BaseActivity
 import com.github.libretube.ui.dialogs.DownloadDialog
 import com.github.libretube.ui.dialogs.DownloadPlaylistDialog
 
-class DownloadActivity: BaseActivity() {
+class DownloadActivity : BaseActivity() {
     override val isDialogActivity: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +24,11 @@ class DownloadActivity: BaseActivity() {
         val videoId = intentData?.getStringExtra(IntentData.videoId)
         val playlistId = intentData?.getStringExtra(IntentData.playlistId)
         if (videoId != null) {
+            supportFragmentManager.setFragmentResultListener(
+                DownloadDialog.DOWNLOAD_DIALOG_DISMISSED_KEY,
+                this
+            ) { _, _ -> finish() }
+
             DownloadDialog().apply {
                 arguments = bundleOf(IntentData.videoId to videoId)
             }.show(supportFragmentManager, null)
