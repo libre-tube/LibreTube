3b18b60ed3b23fc9d108964f45fa930b4d60efa0
Shuvo <nurshuvo51@gmail.com>
2024-05-20 22:24:31 +0600

    fix build

--- app/src/main/java/com/github/libretube/services/OnClearFromRecentService.kt
+++ app/src/main/java/com/github/libretube/services/OnClearFromRecentService.kt
@@ -5,7 +5,6 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import androidx.core.content.getSystemService
-import com.github.libretube.util.NowPlayingNotification.Companion.PLAYER_NOTIFICATION_ID
 
 class OnClearFromRecentService : Service() {
     private var nManager: NotificationManager? = null
@@ -26,5 +25,9 @@ class OnClearFromRecentService : Service() {
     override fun onTaskRemoved(rootIntent: Intent?) {
         nManager?.cancel(PLAYER_NOTIFICATION_ID)
         stopSelf()
+    }
+
+    companion object {
+        private const val PLAYER_NOTIFICATION_ID = 1
     }
 }
