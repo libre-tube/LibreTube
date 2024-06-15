d11b7b17b957d49e52403a8b5049b9f9dbf7baba
Isira Seneviratne <isirasen96@gmail.com>
2024-06-15 05:47:52 +0530

    refactor: Use parcelable extensions with channel tabs

--- app/src/main/java/com/github/libretube/api/obj/ChannelTab.kt
+++ app/src/main/java/com/github/libretube/api/obj/ChannelTab.kt
@@ -1,9 +1,12 @@
 package com.github.libretube.api.obj
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Parcelize
 data class ChannelTab(
     val name: String,
     val data: String
-)
+) : Parcelable
