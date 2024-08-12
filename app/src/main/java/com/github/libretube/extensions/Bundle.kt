acf73aab98e0fa4bb63a2bea6305aebaa1d2ad8c
Isira Seneviratne <isirasen96@gmail.com>
2024-08-12 06:41:22 +0530

    refactor: Simplify serializable extensions

--- app/src/main/java/com/github/libretube/extensions/Bundle.kt
+++ app/src/main/java/com/github/libretube/extensions/Bundle.kt
@@ -1,11 +1,9 @@
 package com.github.libretube.extensions
 
-import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import androidx.core.os.BundleCompat
 import java.io.Serializable
-import kotlin.reflect.safeCast
 
 inline fun <reified T : Parcelable> Bundle.parcelable(key: String?): T? {
     return BundleCompat.getParcelable(this, key, T::class.java)
@@ -16,14 +14,5 @@ inline fun <reified T : Parcelable> Bundle.parcelableArrayList(key: String?): Ar
 }
 
 inline fun <reified T : Serializable> Bundle.serializable(key: String?): T? {
-    return getSerializable(this, key, T::class.java)
-}
-
-fun <T : Serializable> getSerializable(bundle: Bundle, key: String?, clazz: Class<T>): T? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-        bundle.getSerializable(key, clazz)
-    } else {
-        @Suppress("DEPRECATION")
-        clazz.kotlin.safeCast(bundle.getSerializable(key))
-    }
+    return BundleCompat.getSerializable(this, key, T::class.java)
 }
