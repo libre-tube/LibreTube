fa788d7d50ae7d244fdc7f63c59cea0eb36f9b6a
Thomas W. <thomasdewitt@outlook.com>
2025-01-14 15:24:25 +0100

    Pop from replies to comments instead of closing comments sheet

--- app/src/main/java/com/github/libretube/ui/sheets/CommentsSheet.kt
+++ app/src/main/java/com/github/libretube/ui/sheets/CommentsSheet.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.ComponentDialog
+import androidx.activity.addCallback
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
