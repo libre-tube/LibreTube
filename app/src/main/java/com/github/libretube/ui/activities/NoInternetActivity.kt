605ce628ecd9f5816f7bbac4748551c7731a6ea1
Thomas W. <thomasdewitt@outlook.com>
2025-01-15 15:41:09 +0100

    Migrate to FragmentContainerView

--- app/src/main/java/com/github/libretube/ui/activities/NoInternetActivity.kt
+++ app/src/main/java/com/github/libretube/ui/activities/NoInternetActivity.kt
@@ -2,8 +2,6 @@ package com.github.libretube.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.navigation.findNavController
-import com.github.libretube.R
 import com.github.libretube.constants.IntentData
 import com.github.libretube.databinding.ActivityNointernetBinding
 import com.github.libretube.helpers.NavigationHelper
@@ -15,9 +13,6 @@ class NoInternetActivity : BaseActivity() {
 
         val binding = ActivityNointernetBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navController = findNavController(R.id.fragment)
-        navController.graph = navController.navInflater.inflate(R.navigation.nav_nointernet)
     }
 
     override fun onNewIntent(intent: Intent) {
