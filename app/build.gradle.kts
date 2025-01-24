86a00281e159a5134198615b8fe9cc398bb664fe
Bnyro <bnyro@tutanota.com>
2025-01-24 20:13:26 +0100

    chore: bump version to v0.27.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.libretube"
         minSdk = 21
         targetSdk = 34
-        versionCode = 58
-        versionName = "0.26.0"
+        versionCode = 59
+        versionName = "0.27.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "LibreTube")
 
