2ae689c74d818dfcee777a6b04a2a2dae68f001a
Thomas W. <thomasdewitt@outlook.com>
2025-02-15 14:54:21 +0100

    refactor: simplify add to playlist dialog (#7074)

--- app/src/main/java/com/github/libretube/api/obj/Playlists.kt
+++ app/src/main/java/com/github/libretube/api/obj/Playlists.kt
@@ -1,12 +1,15 @@
 package com.github.libretube.api.obj
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Parcelize
 data class Playlists(
     val id: String? = null,
     var name: String? = null,
     var shortDescription: String? = null,
     val thumbnail: String? = null,
     val videos: Long = 0
-)
+) : Parcelable
