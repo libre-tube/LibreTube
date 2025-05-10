6fb0dc7007d0e42cea387211f191a5a4ebf53bab
FineFindus <FineFindus@proton.me>
2025-05-10 11:15:12 +0200

    chore: remove leftover legacy subscription views

--- app/src/main/java/com/github/libretube/ui/viewholders/LegacySubscriptionViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.github.libretube.ui.viewholders
-
-import androidx.recyclerview.widget.RecyclerView
-import com.github.libretube.databinding.LegacySubscriptionChannelBinding
-
-class LegacySubscriptionViewHolder(
-    val binding: LegacySubscriptionChannelBinding
-) : RecyclerView.ViewHolder(binding.root)
