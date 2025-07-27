libre-tube/LibreTube

commit 4407b26754f516b99b714c5940296515495153c6

feat: remove download title
Hides the download title, as it is not otherwise exposed to the user and just
used for internally storing the downloaded items.

app/src/main/java/com/github/libretube/ui/dialogs/DownloadDialog.kt
@@ -19,6 +19,7 @@ import com.github.libretube.api.obj.Subtitle
 import com.github.libretube.constants.IntentData
 import com.github.libretube.databinding.DialogDownloadBinding
 import com.github.libretube.extensions.getWhileDigit
+import com.github.libretube.extensions.sha256Sum
 import com.github.libretube.extensions.toastFromMainDispatcher
 import com.github.libretube.helpers.DownloadHelper
 import com.github.libretube.helpers.PlayerHelper
@@ -44,25 +45,6 @@ class DownloadDialog : DialogFragment() {
         val binding = DialogDownloadBinding.inflate(layoutInflater)
 
         fetchAvailableSources(binding)
-
-        binding.fileName.filters += InputFilter { source, start, end, _, _, _ ->
-            if (source.isNullOrBlank()) {
-                return@InputFilter null
-            }
-
-            // Extract actual source
-            val actualSource = source.subSequence(start, end)
-            // Filter out unsupported characters
-            val filtered = actualSource.filterNot {
-                TextUtils.RESERVED_CHARS.contains(it, true)
-            }
-            // Check if something was filtered out
-            return@InputFilter if (actualSource.length != filtered.length) {
-                filtered
-            } else {
-                null
-            }
-        }
 
         return MaterialAlertDialogBuilder(requireContext())
             .setTitle(R.string.download)
@@ -94,8 +76,6 @@ class DownloadDialog : DialogFragment() {
     }
 
     private fun initDownloadOptions(binding: DialogDownloadBinding, streams: Streams) {
-        binding.fileName.setText(streams.title)
-
         val videoStreams = streams.videoStreams.filter {
             !it.url.isNullOrEmpty()
         }.filter { !it.format.orEmpty().contains("HLS") }.sortedByDescending {
@@ -144,16 +124,10 @@ class DownloadDialog : DialogFragment() {
         restorePreviousSelections(binding, videoStreams, audioStreams, subtitles)
 
         onDownloadConfirm = onDownloadConfirm@{
-            val fileName = binding.fileName.text.toString()
-            if (fileName.isBlank()) {
-                Toast.makeText(context, R.string.invalid_filename, Toast.LENGTH_SHORT).show()
-                return@onDownloadConfirm
-            }
-
-            if (fileName.toByteArray().size > MAX_FILE_NAME_BYTES - 32) { // reserve 32 bytes for quality and extension
-                Toast.makeText(context, R.string.filename_too_long, Toast.LENGTH_SHORT).show()
-                return@onDownloadConfirm
-            }
+            var fileName = videoId.filter { TextUtils.RESERVED_CHARS.contains(it) }
+                .ifBlank { videoId.sha256Sum() }
+                // reserve 32 bytes for quality and extension
+                .take(MAX_FILE_NAME_BYTES - 32);
 
             val videoPosition = binding.videoSpinner.selectedItemPosition - 1
             val audioPosition = binding.audioSpinner.selectedItemPosition - 1
@@ -172,7 +146,7 @@ class DownloadDialog : DialogFragment() {
 
             val downloadData = DownloadData(
                 videoId = videoId,
-                fileName = binding.fileName.text?.toString().orEmpty(),
+                fileName = fileName,
                 videoFormat = videoStream?.format,
                 videoQuality = videoStream?.quality,
                 audioFormat = audioStream?.format,

app/src/main/res/layout/dialog_download.xml
@@ -6,20 +6,6 @@
     android:orientation="vertical"
     android:paddingHorizontal="20dp">
 
-    <com.google.android.material.textfield.TextInputLayout
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content"
-        android:layout_marginTop="5dp"
-        android:layout_marginBottom="10dp"
-        android:hint="@string/filename">
-
-        <com.google.android.material.textfield.TextInputEditText
-            android:id="@+id/fileName"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:inputType="text" />
-    </com.google.android.material.textfield.TextInputLayout>
-
     <com.github.libretube.ui.views.DropdownMenu
         android:id="@+id/video_spinner"
         android:layout_width="match_parent"

commit ab96e990ee4fb99963478b9957a99ac2b8cc1b42

refactor: use `videoId` for storing downloaded files
Moves to using the videoId directly as the unique filename identifier, as they
are already unique and do contain any reserved path characters.
This also fixes a regression in 4407b26754f516b99b714c5940296515495153c6,
where the filename would only contain disallowed characters.

app/src/main/java/com/github/libretube/api/obj/PipedStream.kt
@@ -31,15 +31,15 @@ data class PipedStream(
     val audioTrackType: String? = null,
     val audioTrackLocale: String? = null
 ): Parcelable {
-    private fun getQualityString(fileName: String): String {
-        return "${fileName}_${quality?.replace(" ", "_")}_$format." +
+    private fun getQualityString(videoId: String): String {
+        return "${videoId}_${quality?.replace(" ", "_")}_$format." +
             mimeType?.split("/")?.last()
     }
 
-    fun toDownloadItem(fileType: FileType, videoId: String, fileName: String) = DownloadItem(
+    fun toDownloadItem(fileType: FileType, videoId: String) = DownloadItem(
         type = fileType,
         videoId = videoId,
-        fileName = getQualityString(fileName),
+        fileName = getQualityString(videoId),
         path = Path(""),
         url = url?.let { ProxyHelper.unwrapUrl(it) },
         format = format,

app/src/main/java/com/github/libretube/api/obj/Streams.kt
@@ -55,21 +55,21 @@ data class Streams(
     val isLive = livestream || duration <= 0
 
     fun toDownloadItems(downloadData: DownloadData): List<DownloadItem> {
-        val (id, name, videoFormat, videoQuality, audioFormat, audioQuality, audioTrackLocale, subCode) = downloadData
+        val (id, videoFormat, videoQuality, audioFormat, audioQuality, audioTrackLocale, subCode) = downloadData
         val items = mutableListOf<DownloadItem>()
 
         if (!videoQuality.isNullOrEmpty() && !videoFormat.isNullOrEmpty()) {
             val stream = videoStreams.find {
                 it.quality == videoQuality && it.format == videoFormat
             }
-            stream?.toDownloadItem(FileType.VIDEO, id, name)?.let { items.add(it) }
+            stream?.toDownloadItem(FileType.VIDEO, id)?.let { items.add(it) }
         }
 
         if (!audioQuality.isNullOrEmpty() && !audioFormat.isNullOrEmpty()) {
             val stream = audioStreams.find {
                 it.quality == audioQuality && it.format == audioFormat && it.audioTrackLocale == audioTrackLocale
             }
-            stream?.toDownloadItem(FileType.AUDIO, id, name)?.let { items.add(it) }
+            stream?.toDownloadItem(FileType.AUDIO, id)?.let { items.add(it) }
         }
 
         if (!subCode.isNullOrEmpty()) {

app/src/main/java/com/github/libretube/parcelable/DownloadData.kt
@@ -6,7 +6,6 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class DownloadData(
     val videoId: String,
-    val fileName: String,
     val videoFormat: String?,
     val videoQuality: String?,
     val audioFormat: String?,

app/src/main/java/com/github/libretube/services/DownloadService.kt
@@ -118,8 +118,7 @@ class DownloadService : LifecycleService() {
 
         val downloadData = intent?.parcelableExtra<DownloadData>(IntentData.downloadData)
             ?: return START_NOT_STICKY
-        val (videoId, name) = downloadData
-        val fileName = name.ifEmpty { videoId }
+        val videoId = downloadData.videoId
 
         lifecycleScope.launch(coroutineContext) {
             val streams = try {
@@ -134,9 +133,9 @@ class DownloadService : LifecycleService() {
                 return@launch
             }
 
-            storeVideoMetadata(videoId, streams, fileName)
+            storeVideoMetadata(videoId, streams)
 
-            val downloadItems = streams.toDownloadItems(downloadData.copy(fileName = fileName))
+            val downloadItems = streams.toDownloadItems(downloadData)
             for (downloadItem in downloadItems) {
                 start(downloadItem)
             }
@@ -145,8 +144,8 @@ class DownloadService : LifecycleService() {
         return START_NOT_STICKY
     }
 
-    private suspend fun storeVideoMetadata(videoId: String, streams: Streams, fileName: String) {
-        val thumbnailTargetPath = getDownloadPath(DownloadHelper.THUMBNAIL_DIR, fileName)
+    private suspend fun storeVideoMetadata(videoId: String, streams: Streams) {
+        val thumbnailTargetPath = getDownloadPath(DownloadHelper.THUMBNAIL_DIR, videoId)
 
         val download = Download(
             videoId,

app/src/main/java/com/github/libretube/services/PlaylistDownloadEnqueueService.kt
@@ -148,16 +148,8 @@ class PlaylistDownloadEnqueueService : LifecycleService() {
                 val videoStream = getStream(videoInfo.videoStreams, maxVideoQuality)
                 val audioStream = getStream(videoInfo.audioStreams, maxAudioQuality)
 
-                // remove all UNIX reserved characters from the title in order to generate
-                // a valid filename
-                var fileName = videoInfo.title
-                TextUtils.RESERVED_CHARS.forEach {
-                    fileName = fileName.replace(it, '_')
-                }
-
                 val downloadData = DownloadData(
                     videoId = videoId,
-                    fileName = fileName,
                     videoFormat = videoStream?.format,
                     videoQuality = videoStream?.quality,
                     audioFormat = audioStream?.format,

app/src/main/java/com/github/libretube/ui/dialogs/DownloadDialog.kt
@@ -124,11 +124,6 @@ class DownloadDialog : DialogFragment() {
         restorePreviousSelections(binding, videoStreams, audioStreams, subtitles)
 
         onDownloadConfirm = onDownloadConfirm@{
-            var fileName = videoId.filter { TextUtils.RESERVED_CHARS.contains(it) }
-                .ifBlank { videoId.sha256Sum() }
-                // reserve 32 bytes for quality and extension
-                .take(MAX_FILE_NAME_BYTES - 32);
-
             val videoPosition = binding.videoSpinner.selectedItemPosition - 1
             val audioPosition = binding.audioSpinner.selectedItemPosition - 1
             val subtitlePosition = binding.subtitleSpinner.selectedItemPosition - 1
@@ -146,7 +141,6 @@ class DownloadDialog : DialogFragment() {
 
             val downloadData = DownloadData(
                 videoId = videoId,
-                fileName = fileName,
                 videoFormat = videoStream?.format,
                 videoQuality = videoStream?.quality,
                 audioFormat = audioStream?.format,
@@ -237,11 +231,6 @@ class DownloadDialog : DialogFragment() {
     }
 
     companion object {
-        /**
-         * Max file name length at Android systems
-         */
-        private const val MAX_FILE_NAME_BYTES = 255
-
         private const val VIDEO_DOWNLOAD_QUALITY = "video_download_quality"
         private const val VIDEO_DOWNLOAD_FORMAT = "video_download_format"
         private const val AUDIO_DOWNLOAD_QUALITY = "audio_download_quality"