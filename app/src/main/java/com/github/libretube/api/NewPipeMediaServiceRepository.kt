libre-tube/LibreTube

commit eefd23bd3ae1ddc314b40e03b8b9cbdd28cf64bc

feat: filter members-only videos from channel tab
Applies the filtering of members-only videos to the channel page. As some
channels upload a lot/mainly members-only content, it can be a hassle to find
videos that are watchable in the app, which isn't a great user experience.

Ref: https://github.com/libre-tube/LibreTube/pull/7113

app/src/main/java/com/github/libretube/api/NewPipeMediaServiceRepository.kt
@@ -52,6 +52,7 @@ import org.schabi.newpipe.extractor.services.youtube.extractors.YoutubeStreamExt
 import org.schabi.newpipe.extractor.stream.AudioStream
 import org.schabi.newpipe.extractor.stream.StreamInfo
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
+import org.schabi.newpipe.extractor.stream.StreamInfoItem.ContentAvailability
 import org.schabi.newpipe.extractor.stream.VideoStream
 
 
@@ -449,7 +450,9 @@ class NewPipeMediaServiceRepository : MediaServiceRepository {
         }
 
         return ChannelTabResponse(
-            content = items.mapNotNull { it.toContentItem() },
+            content = items.filterIsInstance<StreamInfoItem>()
+                .filter { it.contentAvailability == ContentAvailability.AVAILABLE || it.contentAvailability == ContentAvailability.UPCOMING }
+                .mapNotNull { it.toContentItem() },
             nextpage = newNextPage?.toNextPageString()
         )
     }

commit a0d93dc2a17d2a62c2a53684b78b89edb9c2be43

fix: filter only `StreamInfoItem`s for availability
Fixes a regression in eefd23bd3ae1ddc314b40e03b8b9cbdd28cf64bc, where the
channel response would be filter for `StreamInfoItem`s, which lead to other
items type, such as playlists being erroneously removed.

Closes: https://github.com/libre-tube/LibreTube/issues/7682

app/src/main/java/com/github/libretube/api/NewPipeMediaServiceRepository.kt
@@ -120,7 +120,7 @@ fun StreamInfoItem.toStreamItem(
 }
 
 fun InfoItem.toContentItem() = when (this) {
-    is StreamInfoItem -> ContentItem(
+    is StreamInfoItem -> if (contentAvailability == ContentAvailability.AVAILABLE || contentAvailability == ContentAvailability.UPCOMING) ContentItem(
         url = url.toID(),
         type = TYPE_STREAM,
         thumbnail = thumbnails.maxByOrNull { it.height }?.url.orEmpty(),
@@ -134,7 +134,7 @@ fun InfoItem.toContentItem() = when (this) {
         shortDescription = shortDescription,
         verified = isUploaderVerified,
         duration = duration
-    )
+    ) else null
 
     is ChannelInfoItem -> ContentItem(
         url = url.toID(),
@@ -450,9 +450,7 @@ class NewPipeMediaServiceRepository : MediaServiceRepository {
         }
 
         return ChannelTabResponse(
-            content = items.filterIsInstance<StreamInfoItem>()
-                .filter { it.contentAvailability == ContentAvailability.AVAILABLE || it.contentAvailability == ContentAvailability.UPCOMING }
-                .mapNotNull { it.toContentItem() },
+            content = items.mapNotNull { it.toContentItem() },
             nextpage = newNextPage?.toNextPageString()
         )
     }