553ba4ea44b20f8cbf9cfcd072717cf022986f1e
Elolcol <main.scarring648@passinbox.com>
2025-09-23 19:51:28 -0300

    feat: now the playlist thumbnail has the same content as the playlistRow if it is empty

--- app/src/main/java/com/github/libretube/ui/fragments/PlaylistFragment.kt
+++ app/src/main/java/com/github/libretube/ui/fragments/PlaylistFragment.kt
@@ -11,6 +11,7 @@ import androidx.core.os.bundleOf
 import androidx.core.text.parseAsHtml
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
+import androidx.core.view.setPadding
 import androidx.core.view.updatePadding
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -144,6 +145,12 @@ class PlaylistFragment : DynamicLayoutManagerFragment(R.layout.fragment_playlist
             nextPage = response.nextpage
             playlistName = response.name
             isLoading = false
+
+            if (response.thumbnailUrl.isNullOrEmpty()) {
+                binding.thumbnail.setImageResource(R.drawable.ic_empty_playlist)
+                binding.thumbnail.setPadding(64f.dpToPx())
+                binding.thumbnail.setBackgroundColor(com.google.android.material.R.attr.colorSurface)
+            }
 
             if (!response.thumbnailUrl.isNullOrEmpty())
                 ImageHelper.loadImage(response.thumbnailUrl, binding.thumbnail)
