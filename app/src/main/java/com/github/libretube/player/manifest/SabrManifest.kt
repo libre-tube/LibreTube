e42007e7beb8f5fd6b6108cecc24a07ebeb65cae
FineFindus <FineFindus@proton.me>
2025-10-09 16:16:05 +0200

    feat(sabr): add manifest

---
+++ app/src/main/java/com/github/libretube/player/manifest/SabrManifest.kt
@@ -0,0 +1,123 @@
+package com.github.libretube.player.manifest
+
+import android.net.Uri
+import android.util.Base64
+import androidx.core.net.toUri
+import androidx.media3.common.C
+import androidx.media3.common.C.ROLE_FLAG_DESCRIBES_VIDEO
+import androidx.media3.common.C.ROLE_FLAG_DUB
+import androidx.media3.common.C.ROLE_FLAG_MAIN
+import androidx.media3.common.C.ROLE_FLAG_SUPPLEMENTARY
+import androidx.media3.common.Format
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.StreamKey
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.offline.FilterableManifest
+import com.github.libretube.api.obj.PipedStream
+import com.github.libretube.api.obj.Streams
+
+/**
+ * Represents server adaptive-bitrate streaming media metadata.
+ */
+@UnstableApi
+class SabrManifest(
+    /**
+     * Identifier of the video being streamed.
+     */
+    val videoId: String,
+    /**
+     * URL of the streaming server.
+     */
+    val serverAbrStreamingUri: Uri,
+    /**
+     * Required config for media playback.
+     */
+    val videoPlaybackUstreamerConfig: ByteArray,
+    /**
+     * Proof or Origin token.
+     */
+    var poToken: ByteArray?,
+    /**
+     * The duration of the presentation in milliseconds, or [C.TIME_UNSET] if not applicable.
+     */
+    @JvmField val durationMs: Long,
+) : FilterableManifest<SabrManifest?> {
+    @JvmField var adaptationSets: List<AdaptationSet> = emptyList()
+
+    internal constructor(
+        videoId: String,
+        streams: Streams
+    ) : this(
+        videoId,
+        streams.serverAbrStreamingUrl!!.toUri(),
+        Base64.decode(streams.videoPlaybackUstreamerConfig!!, Base64.URL_SAFE),
+        null,
+        streams.duration * 1000,
+    ) {
+        val videoAdaptionSets = streams.videoStreams.groupBy { it.mimeType }
+            .map { (_, streams) ->
+                AdaptationSet(C.TRACK_TYPE_VIDEO, streams.map {
+                    buildRepresentation(
+                        it,
+                        Format.Builder()
+                            .setCodecs(it.codec)
+                            .setContainerMimeType(it.mimeType)
+                            .setSampleMimeType(MimeTypes.getVideoMediaMimeType(it.codec))
+                            .setAverageBitrate(it.bitrate ?: -1)
+                            .setFrameRate(it.fps?.toFloat() ?: -1f)
+                            .setWidth(it.width ?: -1)
+                            .setHeight(it.height ?: -1).build(),
+                    )
+                })
+            };
+
+        val audioAdaptationSets = streams.audioStreams.groupBy { it.mimeType + it.audioTrackId }
+            .map { (_, streams) ->
+                AdaptationSet(C.TRACK_TYPE_AUDIO, streams.map {
+                    buildRepresentation(
+                        it,
+                        Format.Builder()
+                            .setCodecs(it.codec)
+                            .setContainerMimeType(it.mimeType)
+                            .setSampleMimeType(MimeTypes.getAudioMediaMimeType(it.codec))
+                            .setAverageBitrate(it.bitrate ?: -1)
+                            .setChannelCount(2)
+                            .setLanguage(it.audioTrackId?.substring(0, 2)?: it.audioTrackLocale)
+                            .setRoleFlags(
+                                when (it.audioTrackType?.lowercase()) {
+                                    "descriptive" -> ROLE_FLAG_DESCRIBES_VIDEO
+                                    "original" -> ROLE_FLAG_MAIN
+                                    "dubbed", "auto-dubbed" -> ROLE_FLAG_DUB
+                                    "secondary" -> ROLE_FLAG_SUPPLEMENTARY
+                                    else -> 0
+                                }
+                            )
+                            .build()
+                    )
+                })
+            };
+        adaptationSets = videoAdaptionSets + audioAdaptationSets
+    }
+
+    override fun copy(streamKeys: List<StreamKey>): SabrManifest {
+        return this
+    }
+
+    companion object {
+        private fun buildRepresentation(stream: PipedStream, format: Format) = Representation(
+            format,
+            listOf(BaseUrl("sabr://${stream.itag}/${stream.lastModified}")),
+            SegmentBase.SingleSegmentBase(
+                RangedUri(null, stream.initStart!!.toLong(), stream.initEnd!!.toLong()),
+                1,
+                0,
+                stream.indexStart!!.toLong(),
+                stream.indexEnd!!.toLong()
+            ),
+            stream.itag!!,
+            stream.lastModified!!,
+            null,
+            stream
+        )
+    }
+}
--- app/src/main/java/com/github/libretube/services/OnlinePlayerService.kt
+++ app/src/main/java/com/github/libretube/services/OnlinePlayerService.kt
@@ -11,7 +11,6 @@ import androidx.media3.common.MediaItem.SubtitleConfiguration
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.Player
 import androidx.media3.datasource.DefaultDataSource
-import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.exoplayer.hls.HlsMediaSource
 import com.github.libretube.R
 import com.github.libretube.api.JsonHelper
@@ -35,7 +34,9 @@ import com.github.libretube.helpers.PlayerHelper.getSubtitleRoleFlags
 import com.github.libretube.helpers.ProxyHelper
 import com.github.libretube.parcelable.PlayerData
 import com.github.libretube.util.DeArrowUtil
+import com.github.libretube.player.SABRDataSource
 import com.github.libretube.player.SabrMediaSource
+import com.github.libretube.player.manifest.SabrManifest
 import com.github.libretube.util.PlayingQueue
 import com.github.libretube.util.YoutubeHlsPlaylistParser
 import kotlinx.coroutines.CoroutineScope
@@ -284,10 +285,12 @@ open class OnlinePlayerService : AbstractPlayerService() {
             // SABR
             //TODO: enable only experimentally/in DEBUG mode
             streams.serverAbrStreamingUrl != null && streams.videoPlaybackUstreamerConfig != null -> {
-                val dashManifest = PlayerHelper.createDashSource(streams, this)
-                val sabrMediaSourceFactory = SabrMediaSource.Factory(DefaultDataSource.Factory(this))
+                val sabrMediaSourceFactory = SabrMediaSource.Factory(
+                    SABRDataSource.Factory(),
+                    SabrManifest(videoId, streams)
+                )
                 val mediaItem = createMediaItem(
-                    dashManifest,
+                    streams.serverAbrStreamingUrl.toUri(),
                     "application/vnd.yt-ump",
                     streams
                 )
