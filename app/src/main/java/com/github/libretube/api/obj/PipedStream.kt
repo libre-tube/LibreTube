cb4521ab06a0bd71f03ff52cc5805b153483e822
FineFindus <FineFindus@proton.me>
2025-10-12 11:47:55 +0200

    feat(sabr): support retrieving DRC audio

--- app/src/main/java/com/github/libretube/api/obj/PipedStream.kt
+++ app/src/main/java/com/github/libretube/api/obj/PipedStream.kt
@@ -32,6 +32,7 @@ data class PipedStream(
     val audioTrackLocale: String? = null,
     val itag: Int? = null,
     val lastModified: Long? = null,
+    val isDrc: Boolean? = null,
 ): Parcelable {
     private fun getQualityString(videoId: String): String {
         return "${videoId}_${quality?.replace(" ", "_")}_$format." +
