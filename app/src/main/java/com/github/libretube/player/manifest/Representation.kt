0935f6dc22b63b11862d6192bd49da85074b4628
FineFindus <FineFindus@proton.me>
2025-10-19 14:51:07 +0200

    refactor(sabr): remove JVM field tags

--- app/src/main/java/com/github/libretube/player/manifest/Representation.kt
+++ app/src/main/java/com/github/libretube/player/manifest/Representation.kt
@@ -1,24 +1,17 @@
 package com.github.libretube.player.manifest
 
-import androidx.media3.common.C
 import androidx.media3.common.Format
-import androidx.media3.common.util.Assertions
 import androidx.media3.common.util.UnstableApi
 import com.github.libretube.api.obj.PipedStream
-import com.github.libretube.player.SabrSegmentIndex
 import misc.Common.FormatId
 
 /** A Sabr representation.  */
 @UnstableApi
 class Representation(
     /** The format of the representation.  */
-    @JvmField
     val format: Format,
-    @JvmField
     val itag: Int,
-    @JvmField
     val lastModified: Long,
-    @JvmField
     val xtags: String? = null,
     val stream: PipedStream,
 ) {
