8a4b09d5fd5244c3af2b1c2f8cb4a120d802bfdb
Ali hrera <alihrhera1@gmail.com>
2025-11-12 19:10:04 +0200

    feat: Dismiss chapters sheet when hidden
    Introduces a `BottomSheetListener` to allow communication between the player view and the chapters bottom sheet.
    
    When the chapters sheet is manually swiped down and hidden, it now correctly dismisses itself and notifies the player view to ensure its state is updated.
    
    fix: #7831

--- app/src/main/java/com/github/libretube/ui/sheets/ExpandablePlayerSheet.kt
+++ app/src/main/java/com/github/libretube/ui/sheets/ExpandablePlayerSheet.kt
@@ -1,14 +1,44 @@
 package com.github.libretube.ui.sheets
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
+import android.widget.FrameLayout
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.activityViewModels
 import com.github.libretube.ui.models.CommonPlayerViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 abstract class ExpandablePlayerSheet(@LayoutRes layoutResId: Int) :
     UndimmedBottomSheet(layoutResId) {
     private val commonPlayerViewModel: CommonPlayerViewModel by activityViewModels()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        if (commonPlayerViewModel.isFullscreen.value == true) {
+            // prevent an issue where swiping outside of the bottom sheet would make
+            // the app unresponsive by disabling slide actions to dismiss the bottom sheet in fullscreen
+            dialog.setOnShowListener { dialogInterface ->
+                val bottomSheetDialog = dialogInterface as BottomSheetDialog
+                val bottomSheet =
+                    bottomSheetDialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+                        ?: return@setOnShowListener
+
+                val behavior = BottomSheetBehavior.from(bottomSheet)
+                behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                    override fun onStateChanged(bottomSheet: View, newState: Int) {
+                        if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                            dismissAllowingStateLoss()
+                        }
+                    }
+
+                    override fun onSlide(bottomSheet: View, slideOffset: Float) = Unit
+                })
+            }
+        }
+        return dialog
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
