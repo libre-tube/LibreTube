7efcfc10981635c19e34b19bc5630085a318e865
FineFindus <FineFindus@proton.me>
2025-11-13 09:05:25 +0100

    refactor(sabr/xtags): remove unnecessary field

--- app/src/main/java/com/github/libretube/player/parser/Xtags.kt
+++ app/src/main/java/com/github/libretube/player/parser/Xtags.kt
@@ -4,12 +4,11 @@ import android.util.Base64
 import misc.Common.XTags
 
 class Xtags {
-    private var xtags: XTags? = null
     private var enabledFeatures: List<String> = emptyList()
 
     constructor(xtags: String) {
-        this.xtags = XTags.parseFrom(Base64.decode(xtags, Base64.URL_SAFE))
-        this.xtags?.xtagsList?.filter { it.value == "1" }?.map { it.key }?.let {
+        val xtags = XTags.parseFrom(Base64.decode(xtags, Base64.URL_SAFE))
+        xtags?.xtagsList?.filter { it.value == "1" }?.map { it.key }?.let {
             enabledFeatures = it
         }
     }
