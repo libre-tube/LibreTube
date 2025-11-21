74b44fb7f1b79ccb4e0405729fc07be0b4bef3be
FineFindus <FineFindus@proton.me>
2025-11-21 22:14:29 +0100

    feat(sabr): allow discontinuous playback
    Implements support for discontinuous playback, i.e. starting the media at
    different time or seeking during playback.

--- app/src/main/java/com/github/libretube/player/SabrDataSource.kt
+++ app/src/main/java/com/github/libretube/player/SabrDataSource.kt
@@ -7,8 +7,9 @@ import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.BaseDataSource
 import androidx.media3.datasource.DataSource
 import androidx.media3.datasource.DataSpec
+import com.github.libretube.player.parser.PlaybackRequest
 import com.github.libretube.player.parser.SabrClient
-import misc.Common
+import java.io.IOException
 import java.nio.ByteBuffer
 
 @UnstableApi
@@ -20,14 +21,21 @@ class SabrDataSource() : BaseDataSource(true) {
     }
 
     override fun open(dataSpec: DataSpec): Long {
-        val format = dataSpec.customData as Common.FormatId?
-        val segment = SabrClient.data(format!!.itag)
+        val playbackRequest = dataSpec.customData as PlaybackRequest?
+
+        val segment = runCatching { SabrClient.getNextSegment(playbackRequest!!) }
+            .getOrNull() ?: throw IOException()
         data = ByteBuffer.wrap(segment.data())
         return data!!.remaining().toLong()
-
     }
 
-    override fun getUri(): Uri? = data?.let { SabrClient.url.toUri() }
+    override fun getUri(): Uri? {
+        if (data == null || !data!!.hasRemaining()) {
+            // signal that this data source failed to be opened
+            return null
+        }
+        return SabrClient.url.toUri()
+    }
 
     override fun close() {
         data = null
