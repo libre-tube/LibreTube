5ec1373da926e4703e962a8ac1aea4e738cdce9f
Bnyro <bnyro@tutanota.com>
2026-04-25 15:22:58 +0200

    feat: add api stubs for LibreTube sync API

---
+++ app/src/main/java/com/github/libretube/api/ltsync/obj/DeleteUser.kt
@@ -0,0 +1,12 @@
+package com.github.libretube.api.ltsync.obj
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class DeleteUser(
+    @SerialName(value = "password")
+    val password: String
+)
+
