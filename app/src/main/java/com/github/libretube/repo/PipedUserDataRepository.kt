84130a1b4d8e99bbdffb9c44755e7efe6f0433d5
Bnyro <bnyro@tutanota.com>
2026-05-03 19:47:04 +0200

    feat: delegate all playlist bookmarks/subscription groups/watch history Piped methods to the local database repository as Piped doesn't support any of it

--- app/src/main/java/com/github/libretube/repo/PipedUserDataRepository.kt
+++ app/src/main/java/com/github/libretube/repo/PipedUserDataRepository.kt
@@ -15,6 +15,9 @@ import com.github.libretube.api.obj.StreamItem
 import com.github.libretube.api.obj.Subscribe
 import com.github.libretube.api.obj.Subscription
 import com.github.libretube.api.obj.Token
+import com.github.libretube.api.obj.WatchHistoryEntry
+import com.github.libretube.api.obj.WatchHistoryEntryMetadata
+import com.github.libretube.db.obj.PlaylistBookmark
 import com.github.libretube.extensions.toID
 import com.github.libretube.obj.PipedImportPlaylist
 import retrofit2.HttpException
@@ -23,6 +26,8 @@ class PipedUserDataRepository : UserDataRepository {
     override var requiresLogin: Boolean = true
 
     private fun Message.isOk() = this.message == "ok"
+
+    private val localRepositoryDelegate = LocalUserDataRepository()
 
     override suspend fun register(username: String, password: String): String {
         return RetrofitInstance.pipedAuthApi.register(Login(username, password)).token!!
@@ -64,7 +69,7 @@ class PipedUserDataRepository : UserDataRepository {
     override suspend fun renamePlaylist(playlistId: String, newName: String): Boolean {
         val playlist = EditPlaylistBody(playlistId, newName = newName)
 
-        return RetrofitInstance.pipedAuthApi.renamePlaylist( playlist).isOk()
+        return RetrofitInstance.pipedAuthApi.renamePlaylist(playlist).isOk()
     }
 
     override suspend fun changePlaylistDescription(
@@ -82,7 +87,11 @@ class PipedUserDataRepository : UserDataRepository {
         ).playlistId
     }
 
-    override suspend fun removeFromPlaylist(playlistId: String, videoId: String, index: Int): Boolean {
+    override suspend fun removeFromPlaylist(
+        playlistId: String,
+        videoId: String,
+        index: Int
+    ): Boolean {
         return RetrofitInstance.pipedAuthApi.removeFromPlaylist(
             EditPlaylistBody(playlistId = playlistId, index = index)
         ).isOk()
@@ -141,4 +150,61 @@ class PipedUserDataRepository : UserDataRepository {
     override suspend fun getSubscriptionChannelIds(): List<String> {
         return getSubscriptions().map { it.url.toID() }
     }
+
+    //
+    // Piped doesn't support any of the functionalities below, so we handle them locally instead
+    //
+
+    override suspend fun createSubscriptionGroup(name: String) =
+        localRepositoryDelegate.createSubscriptionGroup(name)
+
+    override suspend fun renameSubscriptionGroup(
+        subscriptionGroupId: String,
+        newName: String
+    ) = localRepositoryDelegate.renameSubscriptionGroup(subscriptionGroupId, newName)
+
+    override suspend fun deleteSubscriptionGroup(subscriptionGroupId: String) =
+        localRepositoryDelegate.deleteSubscriptionGroup(subscriptionGroupId)
+
+    override suspend fun getSubscriptionGroup(subscriptionGroupId: String) =
+        localRepositoryDelegate.getSubscriptionGroup(subscriptionGroupId)
+
+    override suspend fun getSubscriptionGroups() = localRepositoryDelegate.getSubscriptionGroups()
+
+    override suspend fun addToSubscriptionGroup(
+        subscriptionGroupId: String,
+        channelId: String
+    ) = localRepositoryDelegate.addToSubscriptionGroup(subscriptionGroupId, channelId)
+
+    override suspend fun removeFromSubscriptionGroup(
+        subscriptionGroupId: String,
+        channelId: String
+    ) = localRepositoryDelegate.removeFromSubscriptionGroup(subscriptionGroupId, channelId)
+
+    override suspend fun addToWatchHistory(video: WatchHistoryEntry) =
+        localRepositoryDelegate.addToWatchHistory(video)
+
+    override suspend fun updateWatchHistoryEntry(metadata: WatchHistoryEntryMetadata) =
+        localRepositoryDelegate.updateWatchHistoryEntry(metadata)
+
+    override suspend fun removeFromWatchHistory(videoId: String) =
+        localRepositoryDelegate.removeFromWatchHistory(videoId)
+
+    override suspend fun getWatchHistory(page: Int) = localRepositoryDelegate.getWatchHistory(page)
+
+    override suspend fun getFromWatchHistory(videoId: String) =
+        localRepositoryDelegate.getFromWatchHistory(videoId)
+
+    override suspend fun clearWatchHistory() = localRepositoryDelegate.clearWatchHistory()
+
+    override suspend fun getPlaylistBookmarks() = localRepositoryDelegate.getPlaylistBookmarks()
+
+    override suspend fun getPlaylistBookmark(playlistId: String) =
+        localRepositoryDelegate.getPlaylistBookmark(playlistId)
+
+    override suspend fun createPlaylistBookmark(playlist: PlaylistBookmark) =
+        localRepositoryDelegate.createPlaylistBookmark(playlist)
+
+    override suspend fun deletePlaylistBookmark(playlistId: String) =
+        localRepositoryDelegate.deletePlaylistBookmark(playlistId)
 }
