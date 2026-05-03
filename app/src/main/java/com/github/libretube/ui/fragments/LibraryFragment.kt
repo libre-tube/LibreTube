6748c8febd7ef517e45a869e40d4911fca6124c9
Bnyro <bnyro@tutanota.com>
2026-05-03 19:48:37 +0200

    feat: add support for syncing playlist bookmarks

--- app/src/main/java/com/github/libretube/ui/fragments/LibraryFragment.kt
+++ app/src/main/java/com/github/libretube/ui/fragments/LibraryFragment.kt
@@ -21,12 +21,12 @@ import com.github.libretube.api.obj.Playlists
 import com.github.libretube.constants.IntentData
 import com.github.libretube.constants.PreferenceKeys
 import com.github.libretube.databinding.FragmentLibraryBinding
-import com.github.libretube.db.DatabaseHolder
 import com.github.libretube.extensions.TAG
 import com.github.libretube.extensions.ceilHalf
 import com.github.libretube.extensions.dpToPx
 import com.github.libretube.helpers.NavBarHelper
 import com.github.libretube.helpers.PreferenceHelper
+import com.github.libretube.repo.UserDataRepositoryHelper
 import com.github.libretube.ui.adapters.PlaylistBookmarkAdapter
 import com.github.libretube.ui.adapters.PlaylistsAdapter
 import com.github.libretube.ui.base.DynamicLayoutManagerFragment
@@ -145,7 +145,7 @@ class LibraryFragment : DynamicLayoutManagerFragment(R.layout.fragment_library) 
     private fun initBookmarks() {
         lifecycleScope.launch {
             val bookmarks = withContext(Dispatchers.IO) {
-                DatabaseHolder.Database.playlistBookmarkDao().getAll()
+                UserDataRepositoryHelper.userDataRepository.getPlaylistBookmarks()
             }
 
             val binding = _binding ?: return@launch
