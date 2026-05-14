libre-tube/LibreTube

commit 761b94606945d7082ad3924c747355f68925e9ed

feat: add option to disable fast forward on long press player action

app/src/main/java/com/github/libretube/ui/views/CustomExoPlayerView.kt
@@ -1314,7 +1314,7 @@ class CustomExoPlayerView(
     }
 
     override fun onLongPress() {
-        if (!PlayerHelper.doubleTapToSeek) return
+        if (!PlayerHelper.longPressFastForward) return
 
         backgroundBinding.fastForwardView.isVisible = true
         val player = player ?: return

app/src/main/java/com/github/libretube/constants/PreferenceKeys.kt
@@ -70,6 +70,7 @@ object PreferenceKeys {
     const val PLAYER_PINCH_CONTROL = "player_pinch_control"
     const val CAPTIONS_SIZE = "captions_size"
     const val DOUBLE_TAP_TO_SEEK = "double_tap_seek"
+    const val LONG_PRESS_FAST_FORWARD = "long_press_fast_forward"
     const val ALTERNATIVE_PIP_CONTROLS = "alternative_pip_controls"
     const val SKIP_SILENCE = "skip_silence"
     const val AUTOPLAY_COUNTDOWN = "autoplay_countdown"

app/src/main/java/com/github/libretube/helpers/PlayerHelper.kt
@@ -328,6 +328,12 @@ object PlayerHelper {
             true
         )
 
+    val longPressFastForward: Boolean
+        get() = PreferenceHelper.getBoolean(
+            PreferenceKeys.LONG_PRESS_FAST_FORWARD,
+            false
+        )
+
     private val alternativePiPControls: Boolean
         get() = PreferenceHelper.getBoolean(
             PreferenceKeys.ALTERNATIVE_PIP_CONTROLS,

app/src/main/res/values/strings.xml
@@ -545,6 +545,8 @@
     <string name="include_timestamp_in_filename">Include timestamp in filename</string>
     <string name="did_you_mean">Did you mean:</string>
     <string name="showing_results_for">Showing results for:</string>
+    <string name="long_press_fast_forward">Fast forward on long press</string>
+    <string name="long_press_fast_forward_summary">Play video with double speed while tapping and holding the screen.</string>
 
     <!-- Notification channel strings -->
     <string name="download_channel_name">Download Service</string>

app/src/main/res/xml/player_settings.xml
@@ -31,6 +31,13 @@
             app:key="double_tap_seek"
             app:title="@string/double_tap_seek" />
 
+        <SwitchPreferenceCompat
+            android:defaultValue="false"
+            android:icon="@drawable/ic_time_outlined"
+            android:summary="@string/long_press_fast_forward_summary"
+            app:key="long_press_fast_forward"
+            app:title="@string/long_press_fast_forward" />
+
     </PreferenceCategory>
 
     <PreferenceCategory app:title="@string/behavior">

commit 94b334ddc0ef207558cf99d74721f8ff331d83b2

fix(Player): check if correct preference when stopping fast-forward
Fixes a regression introduced in 761b94606945d7082ad3924c747355f68925e9ed,
where it would be possible to start fast-forward, but not stop it, as the
stopping used a different preference key then for starting.

Closes: https://github.com/libre-tube/LibreTube/issues/8356

app/src/main/java/com/github/libretube/ui/views/CustomExoPlayerView.kt
@@ -1356,7 +1356,7 @@ class CustomExoPlayerView(
     }
 
     override fun onLongPressEnd() {
-        if (!PlayerHelper.doubleTapToSeek) return
+        if (!PlayerHelper.longPressFastForward) return
 
         backgroundBinding.fastForwardView.isGone = true
 