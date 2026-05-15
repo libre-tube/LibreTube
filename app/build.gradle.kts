720a9cc8d2dd9043785c7e56b2f57a6d74021ab4
Bnyro <bnyro@tutanota.com>
2026-05-15 10:34:35 +0200

    chore: bump version to v31.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.github.libretube"
         minSdk = 26
         targetSdk = 36
-        versionCode = 67
-        versionName = "31.1"
+        versionCode = 68
+        versionName = "31.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "LibreTube")
     }
