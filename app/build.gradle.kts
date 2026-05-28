45472082aca91481b4b7ed92f86effb9dc9499b9
FineFindus <FineFindus@proton.me>
2026-05-28 21:34:44 +0200

    chore: bump version to v31.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.github.libretube"
         minSdk = 26
         targetSdk = 36
-        versionCode = 69
-        versionName = "31.3"
+        versionCode = 70
+        versionName = "31.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "LibreTube")
     }
