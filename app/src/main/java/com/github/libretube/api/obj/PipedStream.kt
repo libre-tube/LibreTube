bb2fa4fade26fd70dc0e7899541f73f0d81c6813
Bnyro <bnyro@tutanota.com>
2026-06-13 22:09:53 +0200

    refactor(downloads): adapt database scheme for SABR download support

--- app/src/main/java/com/github/libretube/api/obj/PipedStream.kt
+++ app/src/main/java/com/github/libretube/api/obj/PipedStream.kt
@@ -3,7 +3,6 @@ package com.github.libretube.api.obj
 import android.os.Parcelable
 import com.github.libretube.db.obj.DownloadItem
 import com.github.libretube.enums.FileType
-import com.github.libretube.helpers.ProxyHelper
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 import kotlin.io.path.Path
@@ -46,7 +45,6 @@ data class PipedStream(
         videoId = videoId,
         fileName = getQualityString(videoId),
         path = Path(""),
-        url = url?.let { ProxyHelper.unwrapUrl(it) },
         format = format,
         quality = quality,
         language = audioTrackLocale,
