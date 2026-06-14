138cfd99c83d467ddf712141ffe8ee7472464cb9
Bnyro <bnyro@tutanota.com>
2026-06-14 16:38:59 +0200

    refactor(sabr): move media3 format construction to Representation

--- app/src/main/java/com/github/libretube/player/manifest/Representation.kt
+++ app/src/main/java/com/github/libretube/player/manifest/Representation.kt
@@ -1,8 +1,14 @@
 package com.github.libretube.player.manifest
 
+import androidx.media3.common.C.ROLE_FLAG_DESCRIBES_VIDEO
+import androidx.media3.common.C.ROLE_FLAG_DUB
+import androidx.media3.common.C.ROLE_FLAG_MAIN
+import androidx.media3.common.C.ROLE_FLAG_SUPPLEMENTARY
 import androidx.media3.common.Format
+import androidx.media3.common.MimeTypes
 import androidx.media3.common.util.UnstableApi
 import com.github.libretube.api.obj.PipedStream
+import com.github.libretube.player.parser.Xtags
 import misc.Common.FormatId
 
 /** A Sabr representation.  */
@@ -18,4 +24,39 @@ data class Representation(
         .setLastModified(stream.lastModified!!)
         .setXtags(stream.xtags ?: "")
         .build()
+
+    constructor(stream: PipedStream) : this(
+        if (MimeTypes.isVideo(stream.mimeType)) {
+            Format.Builder()
+                .setCodecs(stream.codec)
+                .setContainerMimeType(stream.mimeType)
+                .setSampleMimeType(MimeTypes.getVideoMediaMimeType(stream.codec))
+                .setAverageBitrate(stream.bitrate ?: -1)
+                .setFrameRate(stream.fps?.toFloat() ?: -1f)
+                .setWidth(stream.width ?: -1)
+                .setHeight(stream.height ?: -1).build()
+        } else {
+            Format.Builder()
+                .setCodecs(stream.codec)
+                .setContainerMimeType(stream.mimeType)
+                .setSampleMimeType(MimeTypes.getAudioMediaMimeType(stream.codec))
+                .setAverageBitrate(stream.bitrate ?: -1)
+                .setChannelCount(2)
+                .setLanguage(
+                    stream.audioTrackId?.take(2) ?: Xtags(stream.xtags.orEmpty()).language()
+                    ?: stream.audioTrackLocale
+                )
+                .setRoleFlags(
+                    when (stream.audioTrackType?.lowercase()) {
+                        "descriptive" -> ROLE_FLAG_DESCRIBES_VIDEO
+                        "original" -> ROLE_FLAG_MAIN
+                        "dubbed", "auto-dubbed", "dubbed-auto" -> ROLE_FLAG_DUB
+                        "secondary" -> ROLE_FLAG_SUPPLEMENTARY
+                        else -> 0
+                    }
+                )
+                .build()
+        },
+        stream
+    )
 }
