5709862132c05a930d276ba0baed4bfbdaa3a877
Bnyro <bnyro@tutanota.com>
2026-07-30 13:49:18 +0200

    fix: keyboard shortcuts trigger even when typing in searchbar

--- app/src/main/java/com/github/libretube/ui/activities/MainActivity.kt
+++ app/src/main/java/com/github/libretube/ui/activities/MainActivity.kt
@@ -624,6 +624,9 @@ class MainActivity : AbstractPlayerHostActivity() {
     }
 
     override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        // don't forward key events to the player while the search text input is used
+        if (searchItem.isActionViewExpanded) return false
+
         if (runOnPlayerFragment { this@runOnPlayerFragment.onKeyUp(keyCode, event) }) {
             return true
         }
