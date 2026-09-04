b044cca0fecc49a05871b810ec3af2bf15af320f
FineFindus <FineFindus@proton.me>
2026-09-04 12:29:24 +0200

    feat(Player): hide PiP button, if PiP is disabled
    Hides the PiP button, if the user disabled PictureInPicture-mode, as otherwise
    the button does nothing.
    
    Closes: https://github.com/libre-tube/LibreTube/issues/8716

--- app/src/main/java/com/github/libretube/compat/PictureInPictureCompat.kt
+++ app/src/main/java/com/github/libretube/compat/PictureInPictureCompat.kt
@@ -1,13 +1,25 @@
 package com.github.libretube.compat
 
 import android.app.Activity
+import android.app.AppOpsManager
 import android.content.Context
 import android.content.pm.PackageManager
 
 object PictureInPictureCompat {
-
+    /**
+     * Returns whether the system supports Picture-in-Picture mode.
+     */
     fun isPictureInPictureAvailable(context: Context) =
         context.packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
+
+    /**
+     * Returns whether the user has enabled Picture-in-Picture mode.
+     */
+    fun isPictureInPictureEnabled(context: Context) =
+        (context.getSystemService(Context.APP_OPS_SERVICE) as? AppOpsManager?)?.checkOpNoThrow(
+            AppOpsManager.OPSTR_PICTURE_IN_PICTURE, android.os.Process.myUid(), context.packageName
+        ) == AppOpsManager.MODE_ALLOWED
+
 
     fun isInPictureInPictureMode(activity: Activity) = activity.isInPictureInPictureMode
 
