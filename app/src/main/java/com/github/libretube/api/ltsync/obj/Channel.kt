e087a4bc24eb361acde290d79896438ecf907df5
Bnyro <bnyro@tutanota.com>
2026-09-20 13:27:33 +0200

    refactor(sync server): allow channels without uploader avatar

--- app/src/main/java/com/github/libretube/api/ltsync/obj/Channel.kt
+++ app/src/main/java/com/github/libretube/api/ltsync/obj/Channel.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Channel(
     @SerialName(value = "avatar")
-    val avatar: String,
+    val avatar: String? = null,
 
     @SerialName(value = "id")
     val id: String,
