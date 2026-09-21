ed5b3dcedc08f104a0423abc12926f3298889015
FineFindus <FineFindus@proton.me>
2026-09-21 15:42:15 +0200

    refactor(Channel): use single viewModel for all channel tabs
    This avoids reloading the channel tab when switching between tabs.

--- app/src/main/java/com/github/libretube/ui/fragments/ChannelContentFragment.kt
+++ app/src/main/java/com/github/libretube/ui/fragments/ChannelContentFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import com.github.libretube.R
+import com.github.libretube.api.obj.ChannelTab
+import com.github.libretube.constants.IntentData
 import com.github.libretube.databinding.FragmentChannelContentBinding
 import com.github.libretube.extensions.ceilHalf
+import com.github.libretube.extensions.parcelable
 import com.github.libretube.ui.adapters.SearchResultsAdapter
 import com.github.libretube.ui.base.DynamicLayoutManagerFragment
-import com.github.libretube.ui.models.ChannelTabViewModel
+import com.github.libretube.ui.models.ChannelViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -20,7 +25,7 @@ class ChannelContentFragment : DynamicLayoutManagerFragment(R.layout.fragment_ch
     private var _binding: FragmentChannelContentBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel: ChannelTabViewModel by viewModels()
+    private val viewModel: ChannelViewModel by viewModels({ requireParentFragment() })
     private val adapter = SearchResultsAdapter()
 
     override fun setLayoutManagers(gridItems: Int) {
@@ -35,16 +40,21 @@ class ChannelContentFragment : DynamicLayoutManagerFragment(R.layout.fragment_ch
         super.onViewCreated(view, savedInstanceState)
         binding.channelRecView.adapter = adapter
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            launch {
-                viewModel.pagingData.collectLatest {
-                    adapter.submitData(it)
-                }
-            }
+        val tab = requireArguments().parcelable<ChannelTab>(IntentData.tabData)!!
 
-            launch {
-                adapter.loadStateFlow.collect {
-                    binding.progressBar.isVisible = it.refresh is LoadState.Loading
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.tabData(tab).collectLatest {
+                        adapter.submitData(it)
+                    }
+                }
+
+                launch {
+                    adapter.loadStateFlow.collect {
+                        binding.progressBar.isVisible = it.refresh is LoadState.Loading
+                    }
                 }
             }
         }
--- app/src/main/java/com/github/libretube/ui/models/ChannelViewModel.kt
+++ app/src/main/java/com/github/libretube/ui/models/ChannelViewModel.kt
@@ -1,6 +1,23 @@
 package com.github.libretube.ui.models
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.github.libretube.api.obj.ChannelTab
+import com.github.libretube.api.obj.ContentItem
+import com.github.libretube.ui.models.sources.ChannelTabPagingSource
+import kotlinx.coroutines.flow.Flow
 
 class ChannelViewModel : ViewModel() {
+    private val pagingFlows = mutableMapOf<String, Flow<PagingData<ContentItem>>>()
+
+    fun tabData(tab: ChannelTab): Flow<PagingData<ContentItem>> =
+        pagingFlows.getOrPut(tab.name) {
+            Pager(PagingConfig(pageSize = 20, enablePlaceholders = false)) {
+                ChannelTabPagingSource(tab)
+            }.flow.cachedIn(viewModelScope)
+        }
 }
